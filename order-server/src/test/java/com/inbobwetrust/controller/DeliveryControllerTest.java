42628729c9dbb1f0ac5248d2a0b2dc5bc0e48097
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-09 15:15:03 +0900

    Feat : 배달대행사 라이더배정 Controller

---
+++ order-server/src/test/java/com/inbobwetrust/controller/DeliveryControllerTest.java
@@ -0,0 +1,91 @@
+package com.inbobwetrust.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.inbobwetrust.model.vo.Delivery;
+import com.inbobwetrust.service.DeliveryService;
+
+import static com.inbobwetrust.util.vo.DeliveryInstanceGenerator.makeDeliveryForRequestAndResponse;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DeliveryController.class)
+public class DeliveryControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockBean private DeliveryService deliveryService;
+
+    private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    @DisplayName("사장님 주문접수요청 성공")
+    void addDelivery_successTest() throws Exception {
+        Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
+        Delivery expectedDeliveryResponse = makeDeliveryForRequestAndResponse().get(1);
+        when(this.deliveryService.addDelivery(deliveryRequest))
+                .thenReturn(expectedDeliveryResponse);
+        String requestBody = mapper.writeValueAsString(deliveryRequest);
+
+        MvcResult result =
+                mockMvc.perform(
+                                post("/delivery")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        Delivery responseObj =
+                mapper.readValue(result.getResponse().getContentAsString(), Delivery.class);
+        assertEquals(expectedDeliveryResponse.getOrderId(), responseObj.getOrderId());
+        assertEquals(expectedDeliveryResponse.getRiderId(), responseObj.getRiderId());
+        assertEquals(
+                expectedDeliveryResponse.getWantedPickupTime(), responseObj.getWantedPickupTime());
+        assertEquals(
+                expectedDeliveryResponse.getEstimatedDeliveryFinishTime(),
+                responseObj.getEstimatedDeliveryFinishTime());
+    }
+
+    @Test
+    @DisplayName("배달대행사 라이더배정 테스트")
+    void setRider_successTest() throws Exception {
+        Delivery dlvry = makeDeliveryForRequestAndResponse().get(0);
+        when(this.deliveryService.setRider(dlvry)).thenReturn(dlvry);
+        String requestBody = mapper.writeValueAsString(dlvry);
+
+        MvcResult result =
+                mockMvc.perform(
+                                put("/delivery/rider")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.orderId", is(dlvry.getOrderId())))
+                        .andExpect(jsonPath("$.riderId", is(dlvry.getRiderId())))
+                        .andExpect(jsonPath("$.deliveryAgentId", is(dlvry.getDeliveryAgentId())))
+                        .andReturn();
+    }
+}
