3bb0ef6dfeafa984d690339c52a90346e5e2c456
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-10 00:17:55 +0900

    Fix : Pull Request #7 분리 과정에서 발생한 충돌 해결

--- order-server/src/test/java/com/inbobwetrust/util/vo/DeliveryInstanceGenerator.java
+++ order-server/src/test/java/com/inbobwetrust/util/vo/DeliveryInstanceGenerator.java
@@ -10,32 +10,33 @@ public class DeliveryInstanceGenerator {
         LocalDateTime wantedPickupTime = LocalDateTime.now().plusMinutes(30);
         LocalDateTime estimatedDeliveryFinishTime = wantedPickupTime.plusMinutes(30);
         Delivery deliveryRequest =
-                Delivery.builder()
-                        .orderId("order-1")
-                        .riderId("rider-1")
-                        .deliveryAgentId("agent-1")
-                        .wantedPickupTime(wantedPickupTime)
-                        .build();
+            Delivery.builder()
+                .orderId("order-1")
+                .riderId("rider-1")
+                .deliveryAgentId("agent-1")
+                .wantedPickupTime(wantedPickupTime)
+                .build();
         Delivery expectedDeliveryResponse =
-                Delivery.builder()
-                        .orderId(deliveryRequest.getOrderId())
-                        .riderId(deliveryRequest.getRiderId())
-                        .deliveryAgentId(deliveryRequest.getDeliveryAgentId())
-                        .wantedPickupTime(deliveryRequest.getWantedPickupTime())
-                        .estimatedDeliveryFinishTime(estimatedDeliveryFinishTime)
-                        .build();
+            Delivery.builder()
+                .orderId(deliveryRequest.getOrderId())
+                .riderId(deliveryRequest.getRiderId())
+                .deliveryAgentId(deliveryRequest.getDeliveryAgentId())
+                .wantedPickupTime(deliveryRequest.getWantedPickupTime())
+                .estimatedDeliveryFinishTime(estimatedDeliveryFinishTime)
+                .build();
         return List.of(deliveryRequest, expectedDeliveryResponse);
     }
 
     public static Delivery makeSimpleNumberedDelivery(int num) {
         LocalDateTime now = LocalDateTime.now();
         Delivery delivery =
-                Delivery.builder()
-                        .orderId("order-" + num)
-                        .deliveryAgentId("agent-" + num)
-                        .wantedPickupTime(now.plusMinutes(30))
-                        .estimatedDeliveryFinishTime(now.plusMinutes(60))
-                        .build();
+            Delivery.builder()
+                .orderId("order-" + num)
+                .riderId("rider-" + num)
+                .deliveryAgentId("agent-" + num)
+                .wantedPickupTime(now.plusMinutes(30))
+                .estimatedDeliveryFinishTime(now.plusMinutes(60))
+                .build();
         return delivery;
     }
 }
