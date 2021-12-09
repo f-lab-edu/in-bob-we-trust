d25e88e9f57f974a7cd7621d8f0c3af923bb2338
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-10 00:21:56 +0900

    Fix : Pull Request #7 분리 과정에서 발생한 충돌 해결

--- order-server/src/test/java/com/inbobwetrust/util/vo/DeliveryInstanceGenerator.java
+++ order-server/src/test/java/com/inbobwetrust/util/vo/DeliveryInstanceGenerator.java
@@ -3,8 +3,29 @@ package com.inbobwetrust.util.vo;
 import com.inbobwetrust.model.vo.Delivery;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class DeliveryInstanceGenerator {
+    public static List<Delivery> makeDeliveryForRequestAndResponse() {
+        LocalDateTime wantedPickupTime = LocalDateTime.now().plusMinutes(30);
+        LocalDateTime estimatedDeliveryFinishTime = wantedPickupTime.plusMinutes(30);
+        Delivery deliveryRequest =
+            Delivery.builder()
+                .orderId("order-1")
+                .riderId("rider-1")
+                .deliveryAgentId("agent-1")
+                .wantedPickupTime(wantedPickupTime)
+                .build();
+        Delivery expectedDeliveryResponse =
+            Delivery.builder()
+                .orderId(deliveryRequest.getOrderId())
+                .riderId(deliveryRequest.getRiderId())
+                .deliveryAgentId(deliveryRequest.getDeliveryAgentId())
+                .wantedPickupTime(deliveryRequest.getWantedPickupTime())
+                .estimatedDeliveryFinishTime(estimatedDeliveryFinishTime)
+                .build();
+        return List.of(deliveryRequest, expectedDeliveryResponse);
+    }
 
     public static Delivery makeSimpleNumberedDelivery(int num) {
         LocalDateTime now = LocalDateTime.now();
