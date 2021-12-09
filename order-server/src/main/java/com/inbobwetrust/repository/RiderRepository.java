9b8742c1a4714555d4406ae76e89a4c979e028f7
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-09 20:21:27 +0900

    Feat : 라이더위치 Repository 구현

--- order-server/src/main/java/com/inbobwetrust/repository/RiderRepository.java
+++ order-server/src/main/java/com/inbobwetrust/repository/RiderRepository.java
@@ -2,11 +2,15 @@ package com.inbobwetrust.repository;
 
 import com.inbobwetrust.model.vo.Rider;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RiderRepository {
+    boolean save(Rider rider);
 
     boolean update(Rider rider);
 
     Optional<Rider> findByRiderId(String riderId);
+
+    List<Rider> findAll();
 }
