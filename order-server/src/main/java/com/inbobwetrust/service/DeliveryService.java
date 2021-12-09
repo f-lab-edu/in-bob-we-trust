54b68f4e1bee0aab53fa8a6e83bfac82f9250672
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-09 17:49:33 +0900

    Feat : 배달완료 Controller 구현

---
+++ order-server/src/main/java/com/inbobwetrust/service/DeliveryService.java
@@ -0,0 +1,97 @@
+package com.inbobwetrust.service;
+
+import com.inbobwetrust.model.vo.Delivery;
+import com.inbobwetrust.producer.DeliveryProducer;
+import com.inbobwetrust.repository.DeliveryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.Validator;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryService {
+    private final DeliveryRepository deliveryRepository;
+    private final DeliveryProducer deliveryProducer;
+
+    public Delivery addDelivery(Delivery delivery) {
+        addEstimatedDeliveryFinishTime(delivery);
+        if (!deliveryRepository.save(delivery)) {
+            throw new RuntimeException("Save Operation Failed : delivery with such ID exists");
+        }
+        Optional<Delivery> savedDelivery = deliveryRepository.findByOrderId(delivery.getOrderId());
+        if (savedDelivery.isEmpty()) {
+            throw new RuntimeException("Cannot find saved Delivery");
+        }
+        deliveryProducer.sendAddDeliveryMessage(savedDelivery.get());
+        return savedDelivery.get();
+    }
+
+    public void addEstimatedDeliveryFinishTime(Delivery delivery) {
+        delivery.setEstimatedDeliveryFinishTime(delivery.getWantedPickupTime().plusMinutes(30));
+    }
+
+    public Delivery setRider(Delivery delivery) {
+        validateSetRider(delivery);
+        updateOrThrow(delivery, "setRider() Failed : No Such OrderId");
+        Delivery updatedDelivery = findByOrderId(delivery.getOrderId());
+        deliveryProducer.sendSetRiderMessage(updatedDelivery);
+        return updatedDelivery;
+    }
+
+    public Delivery updateDeliveryStatusPickup(Delivery delivery) {
+        validateSetStatusToPickup(delivery);
+        updateOrThrow(delivery, "setStatusToPickup() Failed : No Such OrderId");
+        Delivery updatedDelivery = findByOrderId(delivery.getOrderId());
+        deliveryProducer.sendSetStatusPickupMessage(updatedDelivery);
+        return updatedDelivery;
+    }
+
+    public Delivery updateDeliveryStatusComplete(Delivery deliveryRequest) {
+        return deliveryRequest;
+    }
+
+    private void updateOrThrow(Delivery delivery, String msg) {
+        if (!deliveryRepository.update(delivery)) {
+            throw new RuntimeException(msg);
+        }
+    }
+
+    private void validateSetRider(Delivery delivery) {
+        riderValidation(delivery.getRiderId());
+        agentValidation(delivery.getDeliveryAgentId());
+    }
+
+    private void validateSetStatusToPickup(Delivery delivery) {
+        riderValidation(delivery.getRiderId());
+        agentValidation(delivery.getDeliveryAgentId());
+        statusValidation(delivery.getStatus());
+    }
+
+    private Delivery findByOrderId(String orderId) {
+        Optional<Delivery> updatedDelivery = deliveryRepository.findByOrderId(orderId);
+        if (updatedDelivery.isEmpty()) {
+            throw new RuntimeException("Cannot find updated Delivery");
+        }
+        return updatedDelivery.get();
+    }
+
+    private void riderValidation(String rider) {
+        validateStringOrThrow(rider, "Rider not set for delivery");
+    }
+
+    private void agentValidation(String agent) {
+        validateStringOrThrow(agent, "deliveryAgent not set for delivery");
+    }
+
+    private void statusValidation(String status) {
+        validateStringOrThrow(status, "status not set for delivery");
+    }
+
+    private void validateStringOrThrow(String str, String msg) {
+        if (str == null || str.isBlank() || str.isEmpty()) {
+            throw new IllegalArgumentException(msg);
+        }
+    }
+}
---
+++ order-server/src/test/java/com/inbobwetrust/controller/DeliveryControllerTest.java
@@ -0,0 +1,137 @@
+package com.inbobwetrust.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.inbobwetrust.model.vo.Delivery;
+import com.inbobwetrust.service.DeliveryService;
+
+import static com.inbobwetrust.util.vo.DeliveryInstanceGenerator.makeDeliveryForRequestAndResponse;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DeliveryController.class)
+public class DeliveryControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @MockBean private DeliveryService deliveryService;
+
+    private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    @DisplayName("사장님 주문접수완료 API")
+    void addDelivery_successTest() throws Exception {
+        Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
+        when(this.deliveryService.addDelivery(deliveryRequest)).thenReturn(deliveryRequest);
+        String requestBody = mapper.writeValueAsString(deliveryRequest);
+
+        MvcResult result =
+                mockMvc.perform(
+                                post("/delivery")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.orderId", is(deliveryRequest.getOrderId())))
+                        .andExpect(jsonPath("$.riderId", is(deliveryRequest.getRiderId())))
+                        .andExpect(
+                                jsonPath(
+                                        "$.deliveryAgentId",
+                                        is(deliveryRequest.getDeliveryAgentId())))
+                        .andReturn();
+    }
+
+    @Test
+    @DisplayName("배달대행사 라이더배정 API")
+    void setRider_successTest() throws Exception {
+        Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
+        when(this.deliveryService.setRider(deliveryRequest)).thenReturn(deliveryRequest);
+        String requestBody = mapper.writeValueAsString(deliveryRequest);
+
+        MvcResult result =
+                mockMvc.perform(
+                                put("/delivery/rider")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.orderId", is(deliveryRequest.getOrderId())))
+                        .andExpect(jsonPath("$.riderId", is(deliveryRequest.getRiderId())))
+                        .andExpect(
+                                jsonPath(
+                                        "$.deliveryAgentId",
+                                        is(deliveryRequest.getDeliveryAgentId())))
+                        .andReturn();
+    }
+
+    @Test
+    @DisplayName("라이더 픽업완료 API")
+    void setStatusToPickup_successTest() throws Exception {
+        Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
+        deliveryRequest.setStatus("pickedUp");
+        when(this.deliveryService.updateDeliveryStatusPickup(deliveryRequest))
+                .thenReturn(deliveryRequest);
+        String requestBody = mapper.writeValueAsString(deliveryRequest);
+
+        MvcResult result =
+                mockMvc.perform(
+                                patch("/delivery/status/pickup")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.orderId", is(deliveryRequest.getOrderId())))
+                        .andExpect(jsonPath("$.riderId", is(deliveryRequest.getRiderId())))
+                        .andExpect(jsonPath("$.status", is(deliveryRequest.getStatus())))
+                        .andExpect(
+                                jsonPath(
+                                        "$.deliveryAgentId",
+                                        is(deliveryRequest.getDeliveryAgentId())))
+                        .andReturn();
+    }
+
+    @Test
+    @DisplayName("라이더 배달완료 API")
+    void setStatusToComplete_successTest() throws Exception {
+        Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
+        deliveryRequest.setStatus("complete");
+        when(this.deliveryService.updateDeliveryStatusComplete(deliveryRequest))
+                .thenReturn(deliveryRequest);
+        String requestBody = mapper.writeValueAsString(deliveryRequest);
+
+        MvcResult result =
+                mockMvc.perform(
+                                patch("/delivery/status/complete")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(requestBody))
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.orderId", is(deliveryRequest.getOrderId())))
+                        .andExpect(jsonPath("$.riderId", is(deliveryRequest.getRiderId())))
+                        .andExpect(jsonPath("$.status", is(deliveryRequest.getStatus())))
+                        .andExpect(
+                                jsonPath(
+                                        "$.deliveryAgentId",
+                                        is(deliveryRequest.getDeliveryAgentId())))
+                        .andReturn();
+    }
+}
