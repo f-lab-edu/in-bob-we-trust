1b46a6a85e413eb5570c366517a393117b741f22
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-09 16:13:09 +0900

    Feat : 픽업완료 Controller 구현

---
+++ order-server/src/main/java/com/inbobwetrust/controller/DeliveryController.java
@@ -0,0 +1,34 @@
+package com.inbobwetrust.controller;
+
+import com.inbobwetrust.model.vo.Delivery;
+import com.inbobwetrust.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("delivery")
+@RequiredArgsConstructor
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    @PostMapping
+    public ResponseEntity<Delivery> addDelivery(@RequestBody Delivery deliveryRequest) {
+        Delivery delivery = deliveryService.addDelivery(deliveryRequest);
+        return new ResponseEntity<>(delivery, HttpStatus.OK);
+    }
+
+    @PutMapping("rider")
+    public ResponseEntity<Delivery> setRider(@RequestBody Delivery deliveryRequest) {
+        Delivery delivery = deliveryService.setRider(deliveryRequest);
+        return new ResponseEntity<>(delivery, HttpStatus.OK);
+    }
+
+    @PatchMapping("status/pickup")
+    public ResponseEntity<Delivery> updateDeliveryStatusPickup(
+            @RequestBody Delivery deliveryRequest) {
+        Delivery delivery = deliveryService.updateDeliveryStatusPickup(deliveryRequest);
+        return new ResponseEntity<>(delivery, HttpStatus.OK);
+    }
+}
