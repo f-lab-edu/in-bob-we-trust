9c143a81b840a0ca45a1dd50caba67ddf80e9711
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-09 11:02:03 +0900

    Feat : DeliveryRepository 더미 버전

--- order-server/src/main/java/com/inbobwetrust/repository/DeliveryRepository.java
+++ order-server/src/main/java/com/inbobwetrust/repository/DeliveryRepository.java
@@ -2,10 +2,14 @@ package com.inbobwetrust.repository;
 
 import com.inbobwetrust.model.vo.Delivery;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DeliveryRepository {
-    boolean save(Delivery deliveryRequest);
+    boolean save(Delivery delivery);
 
     Optional<Delivery> findByOrderId(String orderId);
+
+    List<Delivery> findAll();
 }
+
