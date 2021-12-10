e88755951015b3a13ee9809459aa41e6940dfd81
ak2j38 <k2j38@naver.com>
2021-12-11 01:58:21 +0900

    Build : DB 사용을 위한 데이터베이스 설정 파일 추가

---
+++ order-server/src/main/java/com/inbobwetrust/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.inbobwetrust.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.woojoolion.in-bob-we-trust.mapper"})
+@PropertySource("classpath:/application.yml")
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(
+                applicationContext.getResources("classpath:/mapper/**/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
