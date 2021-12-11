00fc458fc4a65e5c74b000138deaca2910ce25a1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-11 13:58:00 +0900

    Refactor : 불필요한 커밋 changes 해결

--- order-server/src/test/java/com/inbobwetrust/service/DeliveryServiceTest.java
+++ order-server/src/test/java/com/inbobwetrust/service/DeliveryServiceTest.java
@@ -37,7 +37,7 @@ public class DeliveryServiceTest {
         initialDelivery.setStatus("picked up");
         when(deliveryRepository.update(initialDelivery)).thenReturn(true);
         when(deliveryRepository.findByOrderId(initialDelivery.getOrderId()))
-                .thenReturn(Optional.of(initialDelivery));
+            .thenReturn(Optional.of(initialDelivery));
 
         Delivery setRiderDelivery = deliveryService.setStatusPickup(initialDelivery);
 
@@ -52,8 +52,8 @@ public class DeliveryServiceTest {
         initialDelivery.setStatus(null);
 
         assertThrows(
-                IllegalArgumentException.class,
-                () -> deliveryService.setStatusPickup(initialDelivery));
+            IllegalArgumentException.class,
+            () -> deliveryService.setStatusPickup(initialDelivery));
 
         verify(deliveryRepository, times(0)).update(any(Delivery.class));
     }
@@ -63,16 +63,16 @@ public class DeliveryServiceTest {
     void setRider_successTest() {
         LocalDateTime now = LocalDateTime.now();
         Delivery initialDelivery =
-                Delivery.builder()
-                        .orderId("order-1")
-                        .riderId("rider-1")
-                        .wantedPickupTime(now.plusMinutes(30))
-                        .estimatedDeliveryFinishTime(now.plusMinutes(60))
-                        .deliveryAgentId("agent-1")
-                        .build();
+            Delivery.builder()
+                .orderId("order-1")
+                .riderId("rider-1")
+                .wantedPickupTime(now.plusMinutes(30))
+                .estimatedDeliveryFinishTime(now.plusMinutes(60))
+                .deliveryAgentId("agent-1")
+                .build();
         when(deliveryRepository.update(initialDelivery)).thenReturn(true);
         when(deliveryRepository.findByOrderId(initialDelivery.getOrderId()))
-                .thenReturn(Optional.of(initialDelivery));
+            .thenReturn(Optional.of(initialDelivery));
 
         Delivery setRiderDelivery = deliveryService.setRider(initialDelivery);
 
@@ -85,12 +85,12 @@ public class DeliveryServiceTest {
     void setRider_failTest() {
         LocalDateTime now = LocalDateTime.now();
         Delivery initialDelivery =
-                Delivery.builder()
-                        .orderId("order-1")
-                        .deliveryAgentId("agent-1")
-                        .wantedPickupTime(now.plusMinutes(30))
-                        .estimatedDeliveryFinishTime(now.plusMinutes(60))
-                        .build();
+            Delivery.builder()
+                .orderId("order-1")
+                .deliveryAgentId("agent-1")
+                .wantedPickupTime(now.plusMinutes(30))
+                .estimatedDeliveryFinishTime(now.plusMinutes(60))
+                .build();
 
         assertThrows(RuntimeException.class, () -> deliveryService.setRider(initialDelivery));
 
@@ -102,15 +102,15 @@ public class DeliveryServiceTest {
     void setRider_failTest2() {
         LocalDateTime now = LocalDateTime.now();
         Delivery initialDelivery =
-                Delivery.builder()
-                        .orderId("order-1")
-                        .riderId("rider-1")
-                        .wantedPickupTime(now.plusMinutes(30))
-                        .estimatedDeliveryFinishTime(now.plusMinutes(60))
-                        .build();
+            Delivery.builder()
+                .orderId("order-1")
+                .riderId("rider-1")
+                .wantedPickupTime(now.plusMinutes(30))
+                .estimatedDeliveryFinishTime(now.plusMinutes(60))
+                .build();
 
         assertThrows(
-                IllegalArgumentException.class, () -> deliveryService.setRider(initialDelivery));
+            IllegalArgumentException.class, () -> deliveryService.setRider(initialDelivery));
 
         verify(deliveryRepository, times(0)).update(any(Delivery.class));
     }
@@ -121,7 +121,7 @@ public class DeliveryServiceTest {
         Delivery expectedDeliveryResponse = makeDeliveryForRequestAndResponse().get(1);
         when(deliveryRepository.save(deliveryRequest)).thenReturn(true);
         when(deliveryRepository.findByOrderId(deliveryRequest.getOrderId()))
-                .thenReturn(Optional.of(expectedDeliveryResponse));
+            .thenReturn(Optional.of(expectedDeliveryResponse));
 
         Delivery deliverySaved = deliveryService.addDelivery(deliveryRequest);
 
@@ -151,8 +151,8 @@ public class DeliveryServiceTest {
         deliveryService.addEstimatedDeliveryFinishTime(delivery);
 
         assertEquals(
-                delivery.getWantedPickupTime().plusMinutes(30),
-                delivery.getEstimatedDeliveryFinishTime());
+            delivery.getWantedPickupTime().plusMinutes(30),
+            delivery.getEstimatedDeliveryFinishTime());
     }
 
     @Test
