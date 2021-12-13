da3d18f64624450b6f2ebbf43f19d7938ff7f687
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-13 20:16:00 +0900

    Feat : ResponseBody 필터 구현

---
+++ order-server/src/main/java/com/inbobwetrust/common/CommonResponseHandler.java
@@ -0,0 +1,32 @@
+package com.inbobwetrust.common;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.util.Arrays;
+
+@ControllerAdvice
+public class CommonResponseHandler implements ResponseBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(
+            Object body,
+            MethodParameter returnType,
+            MediaType selectedContentType,
+            Class selectedConverterType,
+            ServerHttpRequest request,
+            ServerHttpResponse response) {
+        return ApiUtil.makeResponseBody(body);
+    }
+}
