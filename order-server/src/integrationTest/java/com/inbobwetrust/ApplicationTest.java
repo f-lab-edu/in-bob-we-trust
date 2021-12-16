9a17dcb60948a870145936b2b150542ee19134a1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-16 22:29:36 +0900

    Feat : Integration 테스트 sourceSet 추가

---
+++ order-server/src/integrationTest/java/com/inbobwetrust/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.inbobwetrust;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
