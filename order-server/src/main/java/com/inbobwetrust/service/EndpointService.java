e6bd04cb57157ffb17fc7d009fb0d3b2fa05c599
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-17 15:14:25 +0900

    Fix : 테스트 컨텍스트 로드 실패 해결

--- order-server/src/main/java/com/inbobwetrust/service/EndpointService.java
+++ order-server/src/main/java/com/inbobwetrust/service/EndpointService.java
@@ -2,9 +2,25 @@ package com.inbobwetrust.service;
 
 import com.inbobwetrust.model.vo.Delivery;
 import com.inbobwetrust.model.vo.Order;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 public interface EndpointService {
     String findShopEndpoint(Order order);
 
     String findDeliveryAgentEndpoint(Delivery delivery);
+}
+
+@Component
+class EndpointServiceTestImpl implements EndpointService{
+
+    @Override
+    public String findShopEndpoint(Order order) {
+        return null;
+    }
+
+    @Override
+    public String findDeliveryAgentEndpoint(Delivery delivery) {
+        return null;
+    }
 }
