804e157ffbfb5e876535b492047c953a8f2beb9e
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-17 14:56:57 +0900

    Feat : mybatis 설정

--- order-server/src/main/java/com/inbobwetrust/Application.java
+++ order-server/src/main/java/com/inbobwetrust/Application.java
@@ -1,9 +1,11 @@
 package com.inbobwetrust;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.inbobwetrust.repository")
 public class Application {
 
     public static void main(String[] args) {
