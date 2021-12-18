977a4ba7811d825ed3c309aef0b799fc8096d84b
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-18 14:31:06 +0900

    Fix : Out of Scope 변경사항 삭제

--- order-server/src/main/java/com/inbobwetrust/repository/RiderRepository.java
+++ order-server/src/main/java/com/inbobwetrust/repository/RiderRepository.java
@@ -6,11 +6,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RiderRepository {
-    int save(Rider rider);
+    boolean save(Rider rider);
+
+    boolean update(Rider rider);
 
     Optional<Rider> findByRiderId(Long riderId);
 
     List<Rider> findAll();
-
-    int deleteAll();
 }
