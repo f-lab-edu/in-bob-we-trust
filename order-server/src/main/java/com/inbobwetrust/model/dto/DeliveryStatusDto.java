8386835bb2f70e01fcf38b67f50e1eba45e7ede4
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-20 20:28:22 +0900

    Feat : DTO 수정

--- order-server/src/main/java/com/inbobwetrust/model/dto/DeliveryStatusDto.java
+++ order-server/src/main/java/com/inbobwetrust/model/dto/DeliveryStatusDto.java
@@ -6,14 +6,23 @@ import lombok.*;
 
 import org.apache.ibatis.type.Alias;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
+@Setter
 @ToString
-@NoArgsConstructor
-@EqualsAndHashCode
+@RequiredArgsConstructor
 @Alias("DeliveryStatusDto")
 public class DeliveryStatusDto {
+    @NotNull(message = "주문번호는 필수값입니다.")
+    @Min(value = 1, message = "주문번호는 최소 1 이상이어야 합니다.")
+    @Max(Long.MAX_VALUE)
     private Long orderId;
-    private OrderStatus orderStatus;
+
+    @NotNull private OrderStatus orderStatus;
 
     @Builder
     public DeliveryStatusDto(Long orderId, OrderStatus orderStatus) {
