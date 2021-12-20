792fdc3c85ae5adbaf141da6d498394921a23e6b
Kim, Joo Hyuk <beanskobe@gmail.com>
2021-12-20 21:21:42 +0900

    #30 배달상태 업데이트 픽업완료 (#75)
    * Feat : DTO 수정
    
    * Feat : DeliveryStatus Mapper 추가
    
    * Feat : 배달 컨트롤러 픽업완료 API 수정
    
    * Format : Google Java Format
    
    * Fix : EqualsAndHashcode 오버라이드
    
    Co-authored-by: github-actions <>

--- order-server/src/main/java/com/inbobwetrust/model/dto/DeliveryStatusDto.java
+++ order-server/src/main/java/com/inbobwetrust/model/dto/DeliveryStatusDto.java
@@ -6,14 +6,20 @@ import lombok.*;
 
 import org.apache.ibatis.type.Alias;
 
-@Getter
-@ToString
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
 @NoArgsConstructor
-@EqualsAndHashCode
 @Alias("DeliveryStatusDto")
 public class DeliveryStatusDto {
+    @NotNull(message = "주문번호는 필수값입니다.")
+    @Min(value = 1, message = "주문번호는 최소 1 이상이어야 합니다.")
+    @Max(Long.MAX_VALUE)
     private Long orderId;
-    private OrderStatus orderStatus;
+
+    @NotNull private OrderStatus orderStatus;
 
     @Builder
     public DeliveryStatusDto(Long orderId, OrderStatus orderStatus) {
--- order-server/src/main/java/com/inbobwetrust/model/mapper/DeliveryMapper.java
+++ order-server/src/main/java/com/inbobwetrust/model/mapper/DeliveryMapper.java
@@ -2,6 +2,7 @@ package com.inbobwetrust.model.mapper;
 
 import com.inbobwetrust.model.dto.DeliveryCreateDto;
 import com.inbobwetrust.model.dto.DeliverySetRiderDto;
+import com.inbobwetrust.model.dto.DeliveryStatusDto;
 import com.inbobwetrust.model.entity.Delivery;
 
 import org.mapstruct.Mapper;
@@ -18,4 +19,6 @@ public interface DeliveryMapper {
     Delivery fromCreateDtoToEntity(DeliveryCreateDto deliveryCreateDto);
 
     Delivery fromSetRiderDtoToEntity(DeliverySetRiderDto deliverySetRiderDto);
+
+    Delivery fromStatusToEntity(DeliveryStatusDto deliveryStatusDto);
 }
