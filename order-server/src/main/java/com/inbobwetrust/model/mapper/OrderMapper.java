59d6444abdc268bf27fd41d30a8bb998338cdf44
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-20 13:18:06 +0900

    Fix : 주문매퍼 매핑 룰 적용

--- order-server/src/main/java/com/inbobwetrust/model/mapper/OrderMapper.java
+++ order-server/src/main/java/com/inbobwetrust/model/mapper/OrderMapper.java
@@ -4,8 +4,9 @@ import com.inbobwetrust.model.dto.OrderDto;
 import com.inbobwetrust.model.entity.Order;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface OrderMapper {
     Order toEntity(OrderDto orderDto);
 }
