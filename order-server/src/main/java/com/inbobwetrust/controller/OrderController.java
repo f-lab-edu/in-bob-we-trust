d626bd3845b7f7f1ca219f4db045d558bf27adb1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-20 11:33:43 +0900

    Feat : OrderDto 컨트롤러 레이어에 적용

--- order-server/src/main/java/com/inbobwetrust/controller/OrderController.java
+++ order-server/src/main/java/com/inbobwetrust/controller/OrderController.java
@@ -1,22 +1,28 @@
 package com.inbobwetrust.controller;
 
 import com.inbobwetrust.config.swaggerdoc.OrderControllerSwaggerDoc;
+import com.inbobwetrust.model.dto.OrderDto;
 import com.inbobwetrust.model.entity.Order;
+import com.inbobwetrust.model.mapper.OrderMapper;
 import com.inbobwetrust.service.OrderService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("order")
 @RequiredArgsConstructor
+@Validated
 class OrderController implements OrderControllerSwaggerDoc {
 
     private final OrderService orderService;
+    private final OrderMapper orderMapper;
 
     @PostMapping
-    public Order receiveNewOrder(@RequestBody Order newOrder) {
-        return orderService.receiveNewOrder(newOrder);
+    public OrderDto receiveNewOrder(@RequestBody OrderDto orderDto) {
+        orderService.receiveNewOrder(orderMapper.toEntity(orderDto));
+        return orderDto;
     }
 }
