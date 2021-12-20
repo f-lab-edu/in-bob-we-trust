63fa5351287931438f5345f3923075b6fd402536
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-20 21:53:08 +0900

    Feat : 주문상태요청Dto 추가

---
+++ order-server/src/test/java/com/inbobwetrust/model/dto/DeliveryGetStatusDtoTest.java
@@ -0,0 +1,26 @@
+package com.inbobwetrust.model.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.inbobwetrust.model.dto.DeliveryStatusDtoTest.checkViolations;
+
+public class DeliveryGetStatusDtoTest {
+    @Test
+    @DisplayName("[DeliveryGetStatusDto] Validation 정상")
+    void newTest_success1() {
+        DeliveryGetStatusDto deliveryGetStatusDto =
+                DeliveryGetStatusDto.builder().orderId(1L).build();
+
+        Assertions.assertEquals(0, checkViolations(deliveryGetStatusDto).size());
+    }
+
+    @Test
+    @DisplayName("[DeliveryGetStatusDto] Validation 실패 : 주문번호 누락")
+    void newTest_fail1() {
+        DeliveryGetStatusDto deliveryGetStatusDto = DeliveryGetStatusDto.builder().build();
+
+        Assertions.assertEquals(1, checkViolations(deliveryGetStatusDto).size());
+    }
+}
