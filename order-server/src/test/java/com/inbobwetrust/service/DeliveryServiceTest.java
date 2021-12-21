7c2896c9043bdb508098c973675c206c127c49f7
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-21 15:04:51 +0900

    Feat : DeliveryService 신규주문접수 관련 Validtion 반영

--- order-server/src/test/java/com/inbobwetrust/service/DeliveryServiceTest.java
+++ order-server/src/test/java/com/inbobwetrust/service/DeliveryServiceTest.java
@@ -5,24 +5,26 @@ import static com.inbobwetrust.util.vo.DeliveryInstanceGenerator.makeDeliveryFor
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import com.inbobwetrust.exceptions.NoAffectedRowsSqlException;
 import com.inbobwetrust.model.dto.DeliveryStatusDto;
 import com.inbobwetrust.model.entity.Delivery;
 import com.inbobwetrust.model.entity.OrderStatus;
 import com.inbobwetrust.producer.DeliveryProducer;
 import com.inbobwetrust.repository.DeliveryRepository;
 import com.inbobwetrust.util.vo.DeliveryInstanceGenerator;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class DeliveryServiceTest {
     @InjectMocks DeliveryService deliveryService;
 
@@ -67,13 +69,16 @@ public class DeliveryServiceTest {
         verify(deliveryRepository, times(1)).update(any(Delivery.class));
     }
 
+    @Test
     @DisplayName("사장님 주문접수완료 : 성공")
     void addDelivery_successTest() {
         Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
-        Delivery expectedDeliveryResponse = makeDeliveryForRequestAndResponse().get(1);
-        when(deliveryRepository.save(deliveryRequest)).thenReturn(true);
+        deliveryRequest.setPickupTime(LocalDateTime.now().plusMinutes(30));
+        deliveryRequest.setFinishTime(null);
+        deliveryRequest.setOrderStatus(null);
+        when(deliveryRepository.save(any())).thenReturn(true);
         when(deliveryRepository.findByOrderId(deliveryRequest.getOrderId()))
-                .thenReturn(Optional.of(expectedDeliveryResponse));
+                .thenReturn(Optional.of(deliveryRequest));
 
         Delivery deliverySaved = deliveryService.addDelivery(deliveryRequest);
 
@@ -85,10 +90,12 @@ public class DeliveryServiceTest {
     @DisplayName("사장님 주문접수완료 : 주문저장이 안된경우 exception")
     void addDelivery_failTest() {
         Delivery deliveryRequest = makeDeliveryForRequestAndResponse().get(0);
-        Delivery expectedDeliveryResponse = makeDeliveryForRequestAndResponse().get(1);
+        deliveryRequest.setFinishTime(null);
         when(deliveryRepository.save(deliveryRequest)).thenReturn(false);
 
-        assertThrows(RuntimeException.class, () -> deliveryService.addDelivery(deliveryRequest));
+        assertThrows(
+                NoAffectedRowsSqlException.class,
+                () -> deliveryService.addDelivery(deliveryRequest));
 
         verify(deliveryRepository, times(1)).save(deliveryRequest);
         verify(deliveryProducer, times(0)).sendAddDeliveryMessage(deliveryRequest);
@@ -98,11 +105,12 @@ public class DeliveryServiceTest {
     @DisplayName("사장님 주문접수완료 : 예상도착시간추가")
     void addEstimatedDeliveryFinishTimeTest() {
         Delivery delivery = makeDeliveryForRequestAndResponse().get(0);
-        assertNull(delivery.getFinishTime());
+        delivery.setFinishTime(null);
 
+        assertNull(delivery.getFinishTime());
         deliveryService.addEstimatedDeliveryFinishTime(delivery);
 
-        assertEquals(delivery.getPickupTime().plusMinutes(30), delivery.getFinishTime());
+        assertNotNull(delivery.getFinishTime());
     }
 
     @Test
@@ -143,5 +151,69 @@ public class DeliveryServiceTest {
                 deliveryService.findDeliveryStatusByOrderId(expected.getOrderId());
 
         assertEquals(expected.getOrderStatus(), actual.getOrderStatus());
+    }
+
+    @Test
+    @DisplayName("[주문접수완료] 실패 : OrderStatus가 new 외의 상태인 경우 에러 발생")
+    void addDeliveryTest_fail1() {
+        Delivery delivery = makeDeliveryForRequestAndResponse().get(0);
+
+        for (OrderStatus status : OrderStatus.values()) {
+            if (!status.equals(OrderStatus.NEW)) {
+                delivery.setOrderStatus(status);
+                assertThrows(
+                        IllegalStateException.class, () -> deliveryService.addDelivery(delivery));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("[주문접수완료] 실패 : (픽업시간==조리완료시간)이 서비스레이어에서 validation 시점의 시간보다 이전이면 오류")
+    void addDeliveryTest_fail() {
+        Delivery delivery = makeDeliveryForRequestAndResponse().get(0);
+        when(deliveryRepository.save(any())).thenReturn(true);
+        when(deliveryRepository.findByOrderId(any())).thenReturn(Optional.of(delivery));
+        
+        delivery.setOrderStatus(OrderStatus.NEW);
+        delivery.setFinishTime(null);
+        delivery.setPickupTime(LocalDateTime.now().plusSeconds(10));
+        assertDoesNotThrow(() -> deliveryService.addDelivery(delivery));
+
+        delivery.setPickupTime(LocalDateTime.now());
+        assertThrows(IllegalStateException.class, () -> deliveryService.addDelivery(delivery));
+
+        delivery.setPickupTime(LocalDateTime.now().minusSeconds(1));
+        assertThrows(IllegalStateException.class, () -> deliveryService.addDelivery(delivery));
+    }
+
+    @Test
+    @DisplayName("[신규주문접수] 성공: 배달예상시간 어플리케이션이 설정")
+    void addDeliveryTest_success() {
+        when(deliveryRepository.save(any())).thenReturn(true); // stub
+        Delivery input = makeDeliveryForRequestAndResponse().get(0); // setup
+        input.setPickupTime(LocalDateTime.now().plusMinutes(30));
+        input.setFinishTime(null);
+
+        //
+        assertNull(input.getFinishTime()); // execute
+        Delivery output = deliveryService.addDelivery(input);
+
+        //
+        assertNotNull(output.getFinishTime()); // assertions
+        verify(deliveryRepository, times(1)).save(input);
+        verify(deliveryProducer, times(1)).sendAddDeliveryMessage(output);
+    }
+
+    @Test
+    @DisplayName("[신규주문접수] 실패 : 배달예상시간 사전에 미리 설정되어있음")
+    void addDeliveryTest_fail2() {
+        when(deliveryRepository.save(any())).thenReturn(true); // stub
+        Delivery inputFail = makeDeliveryForRequestAndResponse().get(0);
+        inputFail.setFinishTime(LocalDateTime.now().plusMinutes(60));
+        // execute
+        assertThrows(IllegalStateException.class, () -> deliveryService.addDelivery(inputFail));
+        // assert
+        verify(deliveryRepository, times(0)).save(any());
+        verify(deliveryProducer, times(0)).sendAddDeliveryMessage(any());
     }
 }
