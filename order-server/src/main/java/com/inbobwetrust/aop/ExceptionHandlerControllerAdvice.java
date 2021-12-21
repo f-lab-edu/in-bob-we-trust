f2c21c8dfbeb3a55a5a51300c7daff21474d3141
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-21 15:15:15 +0900

    Revert "Refactor : 미사용 Logger 삭제"
    This reverts commit bb109e46195832e74321fe2d01baab407639f2fd.

--- order-server/src/main/java/com/inbobwetrust/aop/ExceptionHandlerControllerAdvice.java
+++ order-server/src/main/java/com/inbobwetrust/aop/ExceptionHandlerControllerAdvice.java
@@ -2,6 +2,8 @@ package com.inbobwetrust.aop;
 
 import static com.inbobwetrust.aop.ApiResult.errorResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -14,6 +16,7 @@ import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(value = {IllegalArgumentException.class})
     public ResponseEntity<?> handleIllegalArgumentException(Exception e) {
