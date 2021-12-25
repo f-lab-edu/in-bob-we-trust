8ee7c37846ad2c02e20e9e450aa7d13d6c35c68c
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-25 20:38:08 +0900

    Feat : 주문접수요청 기능 구현

---
+++ order-server/src/main/java/com/inbobwetrust/publisher/PublisherConfig.java
@@ -0,0 +1,14 @@
+package com.inbobwetrust.publisher;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class PublisherConfig {
+
+  @Bean
+  public WebClient webClient(WebClient.Builder builder) {
+    return builder.build();
+  }
+}
