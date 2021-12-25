6214d88c3c3c46106a6253beef80b203aceceb15
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-25 13:25:41 +0900

    Revert "Merge branch '#82-프로젝트-클린업-2' of https://github.com/JooHyukKim/in-bob-we-trust into #82-프로젝트-클린업-2"
    This reverts commit 7a31c50d057ee59a0cc8df257b43b23ade315227, reversing
    changes made to c0c2cb4c0012fbab521bb17a62e3324654f425c8.

--- order-server/src/main/java/com/inbobwetrust/Application.java
+++ order-server/src/main/java/com/inbobwetrust/Application.java
@@ -1,12 +1,6 @@
 package com.inbobwetrust;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+    public static void main(String[] args) {}
 }
--- order-server/src/main/java/com/inbobwetrust/service/DeliveryService.java
+++
@@ -1,9 +0,0 @@
-package com.inbobwetrust.service;
-
-import com.inbobwetrust.domain.Delivery;
-
-import reactor.core.publisher.Mono;
-
-public interface DeliveryService {
-    Mono<Delivery> addDelivery(Delivery delivery);
-}
