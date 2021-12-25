75e19f32689ad684b6a8e34130966bbc754304df
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-25 13:11:25 +0900

    Feat : 신규주문중계 컨트롤러 생성

---
+++ order-server/src/main/java/com/inbobwetrust/controller/DeliveryController.java
@@ -0,0 +1,25 @@
+package com.inbobwetrust.controller;
+
+import com.inbobwetrust.domain.Delivery;
+import com.inbobwetrust.domain.DeliveryStatus;
+import com.inbobwetrust.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/delivery")
+@RequiredArgsConstructor
+public class DeliveryController {
+  private final DeliveryService deliveryService;
+
+  @PostMapping
+  Mono<Delivery> addDelivery(@RequestBody @Valid Delivery delivery) {
+    return deliveryService.addDelivery(delivery);
+  }
+}
