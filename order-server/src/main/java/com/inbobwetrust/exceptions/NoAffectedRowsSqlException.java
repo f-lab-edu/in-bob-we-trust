c1d82e4b5d9abf02ab01e46aa4a522bca2d1194d
Kim, Joo Hyuk <beanskobe@gmail.com>
2021-12-25 09:28:44 +0900

    #82 프로젝트 클린업 (#84)
    * Chores : 프로젝트 클린업
    
    * Format : Google Java Format
    
    Co-authored-by: github-actions <>

--- order-server/src/main/java/com/inbobwetrust/exceptions/NoAffectedRowsSqlException.java
+++
@@ -1,7 +0,0 @@
-package com.inbobwetrust.exceptions;
-
-public class NoAffectedRowsSqlException extends RuntimeException {
-    public NoAffectedRowsSqlException() {
-        super("No rows affected from operation");
-    }
-}
