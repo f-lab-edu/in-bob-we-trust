3b974373529dea387bccd0da8a4b35477bdbcc21
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-25 20:13:14 +0900

    Feat : Delivery 클래스 필드 형식 추가

--- order-server/src/main/java/com/inbobwetrust/domain/Delivery.java
+++ order-server/src/main/java/com/inbobwetrust/domain/Delivery.java
@@ -3,11 +3,15 @@ package com.inbobwetrust.domain;
 import java.time.LocalDateTime;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
+@Setter
 @ToString
 @EqualsAndHashCode
 @Document
@@ -23,6 +27,9 @@ public class Delivery {
 
   private String agencyId;
 
+  @NotBlank(message = "사장님 아이디는 필수 입력값입니다.")
+  private String shopId;
+
   @NotBlank(message = "고객님의 아이디는 필수 입력값입니다.")
   private String customerId;
 
@@ -37,9 +44,25 @@ public class Delivery {
   private DeliveryStatus deliveryStatus;
 
   @NotNull(message = "주문일시는 필수 입력값입니다.")
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS]",
+      timezone = "Asia/Seoul")
   private LocalDateTime orderTime;
 
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS]",
+      timezone = "Asia/Seoul")
   private LocalDateTime pickupTime;
 
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS]",
+      timezone = "Asia/Seoul")
   private LocalDateTime finishTime;
+
+  public void setId(String id) {
+    this.id = id;
+  }
 }
