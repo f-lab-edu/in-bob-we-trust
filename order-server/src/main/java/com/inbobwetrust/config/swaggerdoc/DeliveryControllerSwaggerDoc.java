d172c6f9aeaacd5e2f62c34fdbc889065b432c99
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-27 21:45:53 +0900

    Fix : DeliveryController의 구현메소드 미스매치

--- order-server/src/main/java/com/inbobwetrust/config/swaggerdoc/DeliveryControllerSwaggerDoc.java
+++ order-server/src/main/java/com/inbobwetrust/config/swaggerdoc/DeliveryControllerSwaggerDoc.java
@@ -23,7 +23,7 @@ public interface DeliveryControllerSwaggerDoc {
   public Mono<Delivery> setComplete(Delivery delivery);
 
   @Operation(summary = "모든 배달정보조회, queryParameter로 page와 size 입력필요.")
-  public Flux<Delivery> getDeliveries(Map<String, Object> queryParams);
+  public Flux<Delivery> getDeliveries(Integer page, Integer size);
 
   @Operation(summary = "주문번호로 배달정보조회")
   public Mono<Delivery> getDelivery(String id);
