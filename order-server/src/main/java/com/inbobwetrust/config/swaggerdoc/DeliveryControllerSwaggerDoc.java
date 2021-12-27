5cf685a687c059e2cc7e299923f6fbd5bc305c81
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-27 11:16:00 +0900

    Feat : Swagger Ui 에서 openapi 로 변환

--- order-server/src/main/java/com/inbobwetrust/config/swaggerdoc/DeliveryControllerSwaggerDoc.java
+++ order-server/src/main/java/com/inbobwetrust/config/swaggerdoc/DeliveryControllerSwaggerDoc.java
@@ -1,32 +1,22 @@
 package com.inbobwetrust.config.swaggerdoc;
 
 import com.inbobwetrust.domain.Delivery;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.web.bind.annotation.RequestBody;
+import io.swagger.v3.oas.annotations.Operation;
 import reactor.core.publisher.Mono;
 
-import javax.validation.Valid;
-
 public interface DeliveryControllerSwaggerDoc {
-  @ApiOperation(value = "신규배달 중계추가", notes = "저장된 배달정보 반환")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "성공", response = Delivery.class)})
+  @Operation(summary = "신규배달 중계추가")
   public Mono<Delivery> addDelivery(Delivery delivery);
 
-  @ApiOperation(value = "사장님 주문접수", notes = "저장된 배달정보 반환")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "성공", response = Delivery.class)})
+  @Operation(summary = "사장님 주문접수")
   public Mono<Delivery> acceptDelivery(Delivery delivery);
 
-  @ApiOperation(value = "라이더배정", notes = "저장된 배달정보 반환")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "성공", response = Delivery.class)})
+  @Operation(summary = "라이더배정")
   public Mono<Delivery> setDeliveryRider(Delivery delivery);
 
-  @ApiOperation(value = "주문픽업", notes = "저장된 배달정보 반환")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "성공", response = Delivery.class)})
+  @Operation(summary = "주문픽업")
   public Mono<Delivery> setPickedUp(Delivery delivery);
 
-  @ApiOperation(value = "배달완료", notes = "저장된 배달정보 반환")
-  @ApiResponses(value = {@ApiResponse(code = 200, message = "성공", response = Delivery.class)})
+  @Operation(summary = "배달완료")
   public Mono<Delivery> setComplete(Delivery delivery);
 }
