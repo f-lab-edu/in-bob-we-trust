416fa119a7a247841c6884477bebe630c1ba08a1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-28 18:40:17 +0900

    Test : add placeholder test

--- order-server/src/fullIntegrationTest/java/com/inbobwetrust/ApplicationTest.java
+++ order-server/src/fullIntegrationTest/java/com/inbobwetrust/ApplicationTest.java
@@ -1,9 +1,13 @@
 package com.inbobwetrust;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ApplicationTest {
-
+  @Test
+  void test() {
+    Assertions.assertEquals(1, 1);
+  }
 }
