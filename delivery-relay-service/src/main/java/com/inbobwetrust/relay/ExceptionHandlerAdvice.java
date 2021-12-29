dee135b88a45f1a36e879a9af9c697afc373df33
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-29 23:35:24 +0900

    Feat : RequestBody 바인딩 오류를 핸들링하는 Advice 추가

---
+++ delivery-relay-service/src/main/java/com/inbobwetrust/relay/ExceptionHandlerAdvice.java
@@ -0,0 +1,28 @@
+package com.inbobwetrust.relay;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlerAdvice {
+
+  @ExceptionHandler(value = {WebExchangeBindException.class})
+  public ResponseEntity<String> handleConstraintViolation(WebExchangeBindException ex) {
+    log.error("{} caught by advice : {} ", ex.getClass().getName(), ex.getMessage(), ex);
+    var error =
+        ex.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .sorted()
+            .collect(Collectors.joining(" / "));
+    log.error("Error is : {}", error);
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
+}
