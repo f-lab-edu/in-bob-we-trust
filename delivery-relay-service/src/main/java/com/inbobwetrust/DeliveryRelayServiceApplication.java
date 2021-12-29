471c98945e36741244a3c3aad2014da7a70dd60e
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-29 13:04:54 +0900

    Feat : delivery-relay-service 모듈 생성

---
+++ delivery-relay-service/src/main/java/com/inbobwetrust/DeliveryRelayServiceApplication.java
@@ -0,0 +1,11 @@
+package com.inbobwetrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRelayServiceApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DeliveryRelayServiceApplication.class, args);
+  }
+}
