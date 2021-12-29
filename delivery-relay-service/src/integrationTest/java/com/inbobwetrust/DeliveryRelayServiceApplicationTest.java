77b6703150baf88697437739e5abaa484c66baad
Kim, Joo Hyuk <beanskobe@gmail.com>
2021-12-29 15:59:33 +0900

    #108 새로운 모듈생성 delivery-relay-service  (#111)
    * CI : 워크플로우 모듈별 분리
    
    * Feat : delivery-relay-service 모듈 생성
    
    * Build : 불필요한 external integration test 삭제 >> integration에서 구현하기
    
    * CI : delivery-relay-service integration 추가
    
    * Test : 의도적 failure 수정

--- delivery-relay-service/src/integrationTest/java/com/inbobwetrust/DeliveryRelayServiceApplicationTest.java
+++ delivery-relay-service/src/integrationTest/java/com/inbobwetrust/DeliveryRelayServiceApplicationTest.java
@@ -2,12 +2,14 @@ package com.inbobwetrust;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class ApplicationTest {
+public class DeliveryRelayServiceApplicationTest {
   @Test
-  void test() {
+  void failTest() {
+    // Arrange
+    // Stub
+    // Act
+    // Assert
     Assertions.assertEquals(1, 1);
   }
 }
---
+++ delivery-relay-service/src/test/java/com/inbobwetrust/DeliveryRelayServiceApplicationTest.java
@@ -0,0 +1,16 @@
+package com.inbobwetrust;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DeliveryRelayServiceApplicationTest {
+  @Test
+  void name() {
+    // Arrange
+    // Stub
+    // Act
+
+    // Assert
+    Assertions.assertEquals(1, 1);
+  }
+}
