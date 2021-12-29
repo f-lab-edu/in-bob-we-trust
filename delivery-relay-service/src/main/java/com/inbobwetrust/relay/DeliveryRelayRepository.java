97bb9fb73681ddc55df84c3c271da2d85be22ebb
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2021-12-29 19:38:54 +0900

    Feat : 요청 수신 및 저장 기능

---
+++ delivery-relay-service/src/main/java/com/inbobwetrust/relay/DeliveryRelayRepository.java
@@ -0,0 +1,7 @@
+package com.inbobwetrust.relay;
+
+import com.inbobwetrust.relay.domain.RelayRequest;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface DeliveryRelayRepository extends ReactiveCrudRepository<RelayRequest, String> {
+}
