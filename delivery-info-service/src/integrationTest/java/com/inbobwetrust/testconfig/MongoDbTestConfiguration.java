cd264974eb23e0e10d77f3cf062494e537ee71e5
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-01 20:27:29 +0900

    Feat : in-memoery MongoDB 랜던 포트 설정

--- delivery-info-service/src/integrationTest/java/com/inbobwetrust/testconfig/MongoDbTestConfiguration.java
+++ delivery-info-service/src/integrationTest/java/com/inbobwetrust/testconfig/MongoDbTestConfiguration.java
@@ -4,18 +4,20 @@ import de.flapdoodle.embed.mongo.config.MongodConfig;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
-import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.SocketUtils;
+
+import java.io.IOException;
 
 @Configuration
 @Profile("test")
 public class MongoDbTestConfiguration {
 
   private static final String IP = "localhost";
-  private static final int PORT = 28017;
+  private static final int PORT = SocketUtils.findAvailableTcpPort();
 
   @Primary
   @Bean
