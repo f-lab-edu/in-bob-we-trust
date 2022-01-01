c886102ae4054fd31649446c5d9cca8563add4d4
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-01 19:58:25 +0900

    Feat : test 를 위한 Configuration 추가

---
+++ delivery-info-service/src/integrationTest/java/com/inbobwetrust/testconfig/MongoDbTestConfiguration.java
@@ -0,0 +1,29 @@
+package com.inbobwetrust.testconfig;
+
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import java.io.IOException;
+
+@Configuration
+@Profile("test")
+public class MongoDbTestConfiguration {
+
+  private static final String IP = "localhost";
+  private static final int PORT = 28017;
+
+  @Primary
+  @Bean
+  public MongodConfig embeddedMongoConfiguration() throws IOException {
+    return MongodConfig.builder()
+        .version(Version.LATEST_NIGHTLY)
+        .net(new Net(IP, PORT, Network.localhostIsIPv6()))
+        .build();
+  }
+}
