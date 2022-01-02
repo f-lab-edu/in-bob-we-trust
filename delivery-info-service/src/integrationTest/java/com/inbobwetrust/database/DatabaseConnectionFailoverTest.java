350a8f0e00119a96308857b2f868f8e71d117432
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-02 14:02:25 +0900

    Refactor : DatabaseConnectionFailoverTest 간소화

--- delivery-info-service/src/integrationTest/java/com/inbobwetrust/database/DatabaseConnectionFailoverTest.java
+++ delivery-info-service/src/integrationTest/java/com/inbobwetrust/database/DatabaseConnectionFailoverTest.java
@@ -1,44 +1,48 @@
 package com.inbobwetrust.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.inbobwetrust.domain.Delivery;
 import com.inbobwetrust.domain.DeliveryStatus;
 import com.inbobwetrust.repository.primary.DeliveryRepository;
 import com.inbobwetrust.repository.secondary.SecondaryDeliveryRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import reactor.test.StepVerifier;
 
-import java.sql.SQLException;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.stream.Stream;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.inbobwetrust.service.DeliveryService.FIXED_DELAY;
 import static com.inbobwetrust.service.DeliveryService.MAX_ATTEMPTS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.*;
 
 @Testcontainers
@@ -48,9 +52,10 @@ import static org.mockito.Mockito.*;
 @ActiveProfiles("integrationtest")
 public class DatabaseConnectionFailoverTest {
   static final Logger LOG = LoggerFactory.getLogger(DatabaseConnectionFailoverTest.class);
-  private static final int MONGO_PORT = 27017;
   private String proxyShopUrl = "/relay/v1/shop";
   private String ADD_DELIVERY_URI = "/api/delivery";
+  private static final String DEFAULT_MONGO_DATABASE = "inbob";
+  private static final String MONGO_IMAGE = "mongo:latest";
 
   private ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
@@ -60,87 +65,71 @@ public class DatabaseConnectionFailoverTest {
 
   @SpyBean SecondaryDeliveryRepository secondaryDeliveryRepository;
 
-  @Container public GenericContainer<?> primaryMongo;
+  @Container static GenericContainer<?> primaryMongo = makeMongoDb("primary");
 
-  @Container public GenericContainer<?> secondaryMongo;
+  @Container static GenericContainer<?> secondaryMongo = makeMongoDb("secondary");
 
   @BeforeEach
   void setUp() {
-    primaryMongo = makeMongoDb("primary", 12345);
-    if (!primaryMongo.isRunning()) {
-      primaryMongo.start();
-    }
-    secondaryMongo = makeMongoDb("secondary", 12346);
-    if (!secondaryMongo.isRunning()) {
-      secondaryMongo.start();
-    }
+    primaryMongo.start();
+    secondaryMongo.start();
     assertTrue(primaryMongo.isRunning() && secondaryMongo.isRunning());
     deliveryRepository.deleteAll().block(Duration.ofSeconds(1));
     secondaryDeliveryRepository.deleteAll().block(Duration.ofSeconds(1));
   }
 
-  GenericContainer makeMongoDb(String name, int hostPort) {
-    return new FixedHostPortGenericContainer("mongo:latest")
-        .withFixedExposedPort(hostPort, MONGO_PORT)
-        .withEnv("MONGO_INITDB_DATABASE", "inbob")
+  static GenericContainer makeMongoDb(String name) {
+    return new GenericContainer(MONGO_IMAGE)
+        .withExposedPorts(MongoProperties.DEFAULT_PORT)
+        .withEnv("MONGO_INITDB_DATABASE", DEFAULT_MONGO_DATABASE)
         .waitingFor(
-            new HttpWaitStrategy().forPort(MONGO_PORT).withStartupTimeout(Duration.ofSeconds(10)));
+            new HttpWaitStrategy()
+                .forPort(MongoProperties.DEFAULT_PORT)
+                .withStartupTimeout(Duration.ofSeconds(10)));
   }
 
-  @ParameterizedTest(name = "#{index} - {displayName} = Test with Argument={0}")
-  @DisplayName("[DB연결 테스트] Docker Container 작동여부 확인, 포트 매핑 다른지 확인하기")
-  @MethodSource("makeDeliveryArgument")
-  void databaseConnectionFailoverTest(final Delivery delivery)
-      throws InterruptedException, SQLException {
-    assertTrue(secondaryMongo.isRunning());
+  @DynamicPropertySource
+  static void datasourceProperties(DynamicPropertyRegistry registry) throws InterruptedException {
     assertTrue(primaryMongo.isRunning());
-    Assertions.assertNotEquals(
-        secondaryMongo.getMappedPort(MONGO_PORT), primaryMongo.getMappedPort(MONGO_PORT));
+    registry.add("spring.data.mongodb.primary.uri", () -> extractSimpleMongoUri(primaryMongo));
+
+    assertTrue(secondaryMongo.isRunning());
+    registry.add("spring.data.mongodb.secondary.uri", () -> extractSimpleMongoUri(secondaryMongo));
   }
 
-  @ParameterizedTest(name = "#{index} - {displayName} = Test with Argument={0}")
-  @DisplayName("[Primary & Secondary DB Connection 테스트]")
-  @MethodSource("makeDeliveryArgument")
-  void databaseConnectionFailoverTest2(final Delivery delivery)
-      throws InterruptedException, SQLException {
-    // given
-    var startCountPrimary = deliveryRepository.count().block();
-    var startCountSecondary = secondaryDeliveryRepository.count().block();
+  private static Object extractSimpleMongoUri(GenericContainer<?> primaryMongo) {
+    return String.format(
+        "mongodb://%s:%d/%s",
+        primaryMongo.getHost(),
+        primaryMongo.getMappedPort(MongoProperties.DEFAULT_PORT),
+        DEFAULT_MONGO_DATABASE);
+  }
 
-    var oneDeliveries = List.of(makeADelivery());
-    var fourDeliveries =
-        List.of(makeADelivery(), makeADelivery(), makeADelivery(), makeADelivery());
-
-    // when
-    deliveryRepository.saveAll(oneDeliveries).collectList().block();
-
-    secondaryDeliveryRepository.saveAll(fourDeliveries).collectList().block();
-
-    // then
-    var primaryStream = deliveryRepository.findAll().collectList();
-    StepVerifier.create(primaryStream)
-        .expectNextMatches(pr -> pr.size() == (startCountPrimary + oneDeliveries.size()))
-        .verifyComplete();
-
-    var secondaryStream = secondaryDeliveryRepository.findAll().collectList();
-    StepVerifier.create(secondaryStream)
-        .expectNextMatches(sc -> sc.size() == (startCountSecondary + fourDeliveries.size()))
-        .verifyComplete();
+  @AfterEach
+  void tearDown() {
+    primaryMongo.stop();
+    secondaryMongo.stop();
   }
 
   @ParameterizedTest(name = "#{index} - {displayName} = Test with Argument={0}")
   @DisplayName("[DB Failover 테스트] Primary 데이터베이스를 다운시키면 Secondary에 저장된다.")
   @MethodSource("makeDeliveryArgument")
-  void databaseConnectionRetryTest(Delivery delivery) {
-    LOG.info("primaryMongo is Running : {}", primaryMongo.isRunning());
-    LOG.info("secondaryMongo is Running : {}", secondaryMongo.isRunning());
+  void databaseConnectionRetryTest(Delivery delivery) throws JsonProcessingException {
     // given
-    primaryMongo.stop();
     delivery.setId(null);
-    var responseTime_minThreshold = FIXED_DELAY.multipliedBy(MAX_ATTEMPTS + 1).toMillis();
-    var startTime = System.currentTimeMillis();
 
+    var responseTime_minThreshold = FIXED_DELAY.multipliedBy(MAX_ATTEMPTS + 1).toMillis();
+
+    primaryMongo.stop();
+
+    stubFor(
+        post(urlPathMatching(proxyShopUrl + "/.*"))
+            .willReturn(
+                aResponse()
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .withBody(mapper.writeValueAsString(delivery))));
     // when
+    var startTime = System.currentTimeMillis();
     var savedDelivery =
         testClient
             .post()
@@ -152,14 +141,15 @@ public class DatabaseConnectionFailoverTest {
             .expectBody(Delivery.class)
             .returnResult()
             .getResponseBody();
+    var endTime = System.currentTimeMillis();
 
     // then
-    var endTime = System.currentTimeMillis();
     var executionTime = endTime - startTime;
-    assertEquals(savedDelivery.getOrderId(), delivery.getOrderId());
     assertTrue(responseTime_minThreshold < executionTime);
 
-    verify(deliveryRepository, times(2)).save(any());
+    assertEquals(savedDelivery.getOrderId(), delivery.getOrderId());
+
+    verify(deliveryRepository, times(1)).save(any());
     verify(secondaryDeliveryRepository, times(1)).save(any());
   }
 
@@ -168,23 +158,6 @@ public class DatabaseConnectionFailoverTest {
   }
 
   static Delivery makeDelivery() {
-    return Delivery.builder()
-        .orderId("order-")
-        .shopId("shop-")
-        .customerId("customer-")
-        .address("서울시 강남구 삼성동 봉은사로 12-41 / number")
-        .phoneNumber("01031583212-")
-        .orderTime(LocalDateTime.now())
-        .build();
-  }
-
-  @Test
-  void spyTest() {
-    deliveryRepository.save(makeADelivery()).block();
-    verify(deliveryRepository, times(1)).save(any());
-  }
-
-  Delivery makeADelivery() {
     return Delivery.builder()
         .orderId("order-")
         .riderId("rider-")
