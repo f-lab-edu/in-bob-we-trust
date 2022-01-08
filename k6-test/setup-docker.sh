f17acb9602d01e1f2973aac8080fefdf4af500c8
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-08 17:16:05 +0900

    Test : add tag

--- k6-test/setup-docker.sh
+++ k6-test/setup-docker.sh
@@ -10,7 +10,7 @@ docker image build \
   ./delivery-info-service
 
 docker run -d \
-  -p 8888:8888 --network=inbobwetrust --log-driver local  -v /var/run/docker.sock:/var/run/docker.sock -e COMMAND_LINE_ARGS_BEFORE='-Dspring.data.mongodb.primary.database=inbobwetrust -Dspring.data.mongodb.primary.uri=mongodb://localhost:27017 -Dspring.data.mongodb.secondary.database=inbobwetrust -Dspring.data.mongodb.secondary.uri=mongodb://localhost:27018'   -e COMMAND_LINE_ARGS_AFTER='--spring.profiles.active=loadtest'   beanskobe/delivery-info-service
+  -p 8888:8888 --network=inbobwetrust --log-driver local  -v /var/run/docker.sock:/var/run/docker.sock -e COMMAND_LINE_ARGS_BEFORE='-Dspring.data.mongodb.primary.database=inbobwetrust -Dspring.data.mongodb.primary.uri=mongodb://localhost:27017 -Dspring.data.mongodb.secondary.database=inbobwetrust -Dspring.data.mongodb.secondary.uri=mongodb://localhost:27018'   -e COMMAND_LINE_ARGS_AFTER='--spring.profiles.active=loadtest'   beanskobe/delivㅣery-info-service
 
 
 docker run -d --network=inbobwetrust -d -p 27017:27017 mongo
