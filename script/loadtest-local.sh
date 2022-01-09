0b3355ccd9d7960d3ba1e141d8765ed79947a9cc
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-10 02:00:32 +0900

    Test : again

--- script/loadtest-local.sh
+++ script/loadtest-local.sh
@@ -10,7 +10,6 @@ docker-compose -f ./k6-test/docker-compose-actions.yml up -d
 
 k6 run ./k6-test/simple-delivery-get.js && k6 run ./k6-test/process_cpu_usage.js
 
-
 K6_PROMETHEUS_REMOTE_URL=http://localhost:9090/api/v1/write \
   ./k6-test/k6 run ./k6-test/simple-delivery-get.js  -o output-prometheus-remote && \
   k6 run ./k6-test/process_cpu_usage.js
