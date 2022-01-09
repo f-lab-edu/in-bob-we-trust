ff0ecceb6d586a6bdb018a3548e0aab4e915a4c0
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-10 01:47:20 +0900

    Chroes : Local 테스트 K6 & Prometheus 연동 script 추가

--- script/loadtest-local.sh
+++ script/loadtest-local.sh
@@ -1,4 +1,3 @@
-
 ./gradlew :delivery-info-service:build --x test --x integrationTest
 
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)  || true
@@ -10,3 +9,8 @@ docker image build -t inbobwetrust/delivery-info-service:loadtest ./delivery-inf
 docker-compose -f ./k6-test/docker-compose-actions.yml up -d
 
 k6 run ./k6-test/simple-delivery-get.js && k6 run ./k6-test/process_cpu_usage.js
+
+
+K6_PROMETHEUS_REMOTE_URL=http://localhost:9090/api/v1/write \
+  ./k6-test/k6 run ./k6-test/simple-delivery-get.js  -o output-prometheus-remote && \
+  k6 run ./k6-test/process_cpu_usage.js
