2100d76bcb27d29e2477246a9f3437c1ac441e33
Kim, Joo Hyuk <beanskobe@gmail.com>
2022-01-11 13:32:23 +0900

    #125 Database Failover 기능 삭제 (#153)
    * Feat : Primary & Secondary repository 를 삭제
    
    * Refactor : DeliveryRepsotiroy 패키지 이동

--- delivery-info-service/src/main/java/com/inbobwetrust/repository/DeliveryRepository.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/repository/DeliveryRepository.java
@@ -1,10 +1,10 @@
-package com.inbobwetrust.repository.primary;
+package com.inbobwetrust.repository;
 
 import com.inbobwetrust.domain.Delivery;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 
-public interface PrimaryDeliveryRepository extends ReactiveMongoRepository<Delivery, String> {
+public interface DeliveryRepository extends ReactiveMongoRepository<Delivery, String> {
   Flux<Delivery> findAllByOrderIdContaining(String id, Pageable pageable);
 }
