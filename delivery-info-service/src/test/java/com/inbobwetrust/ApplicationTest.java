06d4bf2bf6887b1334aeee692f45785a29744ab0
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-11 17:47:38 +0900

    Fix : test 에러

--- delivery-info-service/src/test/java/com/inbobwetrust/ApplicationTest.java
+++ delivery-info-service/src/test/java/com/inbobwetrust/ApplicationTest.java
@@ -1,13 +1,5 @@
 package com.inbobwetrust;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class ApplicationTest {
 
-  @Test
-  void mainTest() {
-    Assertions.assertDoesNotThrow(
-        () -> Application.main(new String[] {}));
-  }
 }
