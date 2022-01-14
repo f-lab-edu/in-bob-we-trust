571c68a5f7c405a08320fe4681d917bc385467b6
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-14 17:36:43 +0900

    Feat : RiderLocationRepository 쿼리 추가

--- rider-location-service/src/main/java/com/inbobwetrust/domain/RiderLocationRepository.java
+++ rider-location-service/src/main/java/com/inbobwetrust/domain/RiderLocationRepository.java
@@ -3,6 +3,7 @@ package com.inbobwetrust.domain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -10,7 +11,19 @@ import reactor.core.publisher.Mono;
 public class RiderLocationRepository {
   private final ReactiveRedisTemplate<String, RiderLocation> locationOperations;
 
+  public Mono<Boolean> setIfAbsent(RiderLocation location) {
+    return locationOperations.opsForValue().setIfAbsent(location.getId(), location);
+  }
+
   public Mono<Boolean> setIfPresent(RiderLocation location) {
     return locationOperations.opsForValue().setIfPresent(location.getId(), location);
+  }
+
+  public Flux<RiderLocation> findAll() {
+    return locationOperations.keys("*").flatMap(locationOperations.opsForValue()::get);
+  }
+
+  public Flux<Boolean> deleteAll() {
+    return locationOperations.keys("*").flatMap(locationOperations.opsForValue()::delete);
   }
 }
--- rider-location-service/src/main/java/com/inbobwetrust/domain/RiderLocationService.java
+++ rider-location-service/src/main/java/com/inbobwetrust/domain/RiderLocationService.java
@@ -2,6 +2,7 @@ package com.inbobwetrust.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -9,8 +10,29 @@ import reactor.core.publisher.Mono;
 public class RiderLocationService {
 
   private final RiderLocationRepository repository;
+  private final DeliveryRepository deliveryRepository;
 
   public Mono<Boolean> setIfPresent(RiderLocation location) {
-    return repository.setIfPresent(location);
+    return repository
+        .setIfPresent(location)
+        .flatMap(isSaved -> returnElseRegisterNew(isSaved, location));
+  }
+
+  private Mono<Boolean> returnElseRegisterNew(Boolean isSaved, RiderLocation location) {
+    return isSaved ? Mono.just(true) : doRegisterNew(location);
+  }
+
+  private Mono<Boolean> doRegisterNew(RiderLocation location) {
+    return deliveryRepository
+        .isPickedUp(location.getDeliveryId())
+        .flatMap(isPickedUp -> setIfPickedUpOrReturnFalse(isPickedUp, location));
+  }
+
+  private Mono<Boolean> setIfPickedUpOrReturnFalse(Boolean isPickedUp, RiderLocation location) {
+    return isPickedUp ? repository.setIfAbsent(location) : Mono.just(Boolean.FALSE);
+  }
+
+  public Flux<RiderLocation> findAll() {
+    return repository.findAll();
   }
 }
