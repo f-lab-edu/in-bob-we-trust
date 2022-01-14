2e834563fdff94b3aa326be022035bfcbb3758ee
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-14 17:37:45 +0900

    Test : 테스트 작성

---
+++ rider-location-service/src/integrationTest/java/com/inbobwetrust/domain/RiderLocationWriteTest.java
@@ -0,0 +1,161 @@
+package com.inbobwetrust.domain;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@AutoConfigureWireMock(port = 0)
+@Testcontainers
+public class RiderLocationWriteTest {
+
+  @Autowired WebTestClient testClient;
+
+  @SpyBean RiderLocationRepository locationRepository;
+
+  @SpyBean RiderLocationService locationService;
+
+  @SpyBean DeliveryRepository deliveryRepository;
+
+  @Container static GenericContainer<?> redisContainer = startRedisContainer();
+
+  private static final int REDIS_DEFAULT_PORT = 6379;
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  private static GenericContainer<?> startRedisContainer() {
+    return new GenericContainer<>("redis:latest").withExposedPorts(REDIS_DEFAULT_PORT);
+  }
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    if (!redisContainer.isRunning()) {
+      redisContainer.start();
+    }
+    var redisContainerPort = redisContainer.getMappedPort(REDIS_DEFAULT_PORT);
+    registry.add("spring.redis.port", () -> Objects.requireNonNull(redisContainerPort));
+  }
+
+  @BeforeEach
+  void setup() throws JsonProcessingException {
+    locationRepository.deleteAll().blockLast();
+    var caches = locationRepository.findAll().collectList().block();
+    assertEquals(0, caches.size());
+  }
+
+  @Test
+  @DisplayName("Cache에 없고 픽업완료상태가 아니므로 false를 리턴해야한다.")
+  void updateLocation_test() throws JsonProcessingException {
+    // given
+    var riderLocation =
+        new RiderLocation(LocalDateTime.now().toString(), "delivery1234zz-false", 23.0f, 190f);
+    stubFor(
+        get(urlMatching("/is-picked-up/delivery1234zz-false"))
+            .willReturn(
+                aResponse()
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .withBody(mapper.writeValueAsString(Boolean.FALSE))
+                    .withStatus(HttpStatus.OK.value())));
+    // when
+    testClient
+        .put()
+        .uri("/rider/location")
+        .bodyValue(riderLocation)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(Boolean.class)
+        .isEqualTo(false);
+    // then
+    var resultStream =
+        locationRepository.findAll().filter(r -> r.getId().equals(riderLocation.getId()));
+    StepVerifier.create(resultStream).expectNextCount(1).verifyComplete();
+  }
+
+  @Test
+  @DisplayName("Cache에는 없지만 픽업완료 상태라면 True를 리턴하고 Cache에 값이 있어야한다.")
+  void updateLocation_test_success() throws JsonProcessingException {
+    // given
+    StepVerifier.create(locationRepository.findAll()).expectNextCount(0).verifyComplete();
+    var riderLocation = new RiderLocation("rider-1234", "delivery1234zz-true", 23.0f, 190f);
+    stubFor(
+        get(urlMatching("/is-picked-up/" + riderLocation.getDeliveryId()))
+            .willReturn(
+                aResponse()
+                    .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .withBody(mapper.writeValueAsString(Boolean.TRUE))
+                    .withStatus(HttpStatus.OK.value())));
+    // when
+    testClient
+        .put()
+        .uri("/rider/location")
+        .bodyValue(riderLocation)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(Boolean.class)
+        .isEqualTo(Boolean.TRUE);
+
+    // then
+    var cache = locationRepository.findAll().collectList().block();
+    System.out.println(cache);
+    verify(locationService, times(1)).setIfPresent(any(RiderLocation.class));
+    verify(locationRepository, times(1)).setIfPresent(riderLocation);
+    verify(deliveryRepository, times(1)).isPickedUp(riderLocation.getDeliveryId());
+  }
+
+  @Test
+  @DisplayName("Cache에 있다면 True를 리턴하고 Cache에 값이 있어야한다.")
+  void updateLocation_test_success2() throws JsonProcessingException {
+    // given
+    var deliveryId = LocalDateTime.now().toString();
+    var riderLocation = new RiderLocation(deliveryId, deliveryId, 23.0f, 190f);
+    var isSaved = locationRepository.setIfAbsent(riderLocation).block();
+    assertTrue(isSaved);
+
+    // when
+    testClient
+        .put()
+        .uri("/rider/location")
+        .bodyValue(riderLocation)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectBody(Boolean.class)
+        .isEqualTo(Boolean.TRUE);
+
+    // then
+    StepVerifier.create(locationRepository.findAll()).expectNext(riderLocation).verifyComplete();
+    verify(locationService, times(1)).setIfPresent(any(RiderLocation.class));
+    verify(locationRepository, times(1)).setIfPresent(riderLocation);
+    verify(deliveryRepository, times(0)).isPickedUp(any());
+  }
+}
--- rider-location-service/src/test/java/com/inbobwetrust/domain/RiderLocationServiceTest.java
+++ rider-location-service/src/test/java/com/inbobwetrust/domain/RiderLocationServiceTest.java
@@ -1,45 +1,91 @@
 package com.inbobwetrust.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class RiderLocationServiceTest {
 
   @InjectMocks RiderLocationService locationService;
 
-  @Mock
-  RiderLocationRepository locationRepository;
+  @Mock RiderLocationRepository locationRepository;
 
-  @Test
-  void setIfPresent_success() {
+  @Mock DeliveryRepository deliveryRepository;
+
+  @ParameterizedTest
+  @DisplayName("캐시에 저장하는 워크플로우")
+  @MethodSource("operations")
+  void setIfPresent_fail_does_not_exist(
+      boolean canSetIfPresent,
+      boolean isPickedUp,
+      boolean canSetIfAbsent,
+      boolean finalResult,
+      int isPickedUpTimes,
+      int setIfAbsentTimes) {
     // given
-    var newlocation = new RiderLocation("rea", 32f, 32f);
+    var riderLocation = new RiderLocation("rider-1234", "delivery-1234", 23.0f, Float.MIN_VALUE);
+
     // when
     when(locationRepository.setIfPresent(any(RiderLocation.class)))
-        .thenReturn(Mono.just(Boolean.TRUE));
-    var stream = locationService.setIfPresent(newlocation);
+        .thenReturn(Mono.just(canSetIfPresent));
+
+    if (!canSetIfPresent) {
+      when(deliveryRepository.isPickedUp(anyString())).thenReturn(Mono.just(isPickedUp));
+    }
+
+    if (isPickedUp && !canSetIfPresent) {
+      when(locationRepository.setIfAbsent(any(RiderLocation.class)))
+          .thenReturn(Mono.just(canSetIfAbsent));
+    }
     // then
-    StepVerifier.create(stream).expectNextMatches(Boolean::booleanValue).verifyComplete();
+    var resultStream = locationService.setIfPresent(riderLocation);
+
+    StepVerifier.create(resultStream).expectNextMatches(res -> res == finalResult).verifyComplete();
+
+    verify(locationRepository, times(1)).setIfPresent(any(RiderLocation.class));
+    verify(deliveryRepository, times(isPickedUpTimes)).isPickedUp(anyString());
+    verify(locationRepository, times(setIfAbsentTimes)).setIfAbsent(any(RiderLocation.class));
+  }
+
+  static Stream<Arguments> operations() {
+    return Stream.of(
+        Arguments.of(true, true, true, true, 0, 0),
+        Arguments.of(true, false, false, true, 0, 0),
+        Arguments.of(false, true, true, true, 1, 1),
+        Arguments.of(false, true, false, false, 1, 1),
+        Arguments.of(false, false, true, false, 1, 0),
+        Arguments.of(false, false, false, false, 1, 0));
   }
 
   @Test
-  void setIfPresent_fail_does_not_exist() {
+  void findAllTest() {
     // given
-    var newlocation = new RiderLocation("rea", 32f, 32f);
+    var locations =
+        List.of(
+            new RiderLocation("rider-1234", "delivery-1234", 23.0f, Float.MIN_VALUE),
+            new RiderLocation("rider-12222", "delivery-12222", 23.0f, Float.MIN_VALUE));
+    var returnValue = Flux.fromIterable(locations);
     // when
-    when(locationRepository.setIfPresent(any(RiderLocation.class)))
-        .thenReturn(Mono.just(Boolean.TRUE));
-    var stream = locationService.setIfPresent(newlocation);
+    when(locationRepository.findAll()).thenReturn(returnValue);
     // then
-    StepVerifier.create(stream).expectNextMatches(Boolean::booleanValue).verifyComplete();
+    StepVerifier.create(locationService.findAll())
+        .expectNext(locations.get(0), locations.get(1))
+        .verifyComplete();
   }
 }
