db60fe73c0f8ddc57da8263539e29f79e45d7236
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-15 21:56:10 +0900

    Feat : docker run sh

---
+++ docker-run-info-service.sh
@@ -0,0 +1,5 @@
+docker run \
+    -p 8888:8888 \
+    -e COMMAND_LINE_ARGS_BEFORE='-Dspring.data.mongodb.database=inbobwetrust -Dspring.data.mongodb.primary.uri=mongodb://localhost:27017'  \
+    -e COMMAND_LINE_ARGS_AFTER='--spring.profiles.active=loadtest'  \
+    --network inbobwetrust
