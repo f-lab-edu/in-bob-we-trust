3ab82ad32c27dfc6dd21f13735a14b6b1d14a3ab
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-15 22:14:45 +0900

    CI : run

--- docker-run-info-service.sh
+++ docker-run-info-service.sh
@@ -1,5 +1,5 @@
 docker run -d \
     -p 8888:8888 \
-    -e COMMAND_LINE_ARGS_BEFORE='-Dspring.data.mongodb.database=inbobwetrust -Dspring.data.mongodb.primary.uri=mongodb://localhost:27017'  \
+    -e COMMAND_LINE_ARGS_BEFORE='-Dspring.data.mongodb.database=inbobwetrust -Dspring.data.mongodb.primary.uri=mongodb://host.docker.internal:27017'  \
     -e COMMAND_LINE_ARGS_AFTER='--spring.profiles.active=loadtest'  \
     inbobwetrust/delivery-info-service:loadtest
