844f34268216b1c22c5bb6a5e8585d90342a4901
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-23 01:54:25 +0900

    CI : run

--- containers/mongo/run-container.sh
+++ containers/mongo/run-container.sh
@@ -5,6 +5,8 @@ sudo dockerd > /dev/null 2>&1 & disown
 
 sudo docker run \
  -p "27017:27017" \
+   -e MONGO_INITDB_ROOT_USERNAME=admin \
+   -e MONGO_INITDB_ROOT_PASSWORD=1234 \
  -v /home/ec2-user/mongo/data:/data/db \
   mongo   > /dev/null 2>&1 & disown
 
