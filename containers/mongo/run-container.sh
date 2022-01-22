08f07f82d50671c22b4dfbd1cc221591fee49752
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-22 22:00:57 +0900

    Fix : build gradle

--- containers/mongo/run-container.sh
+++ containers/mongo/run-container.sh
@@ -1,4 +1,11 @@
+sudo yum install dockerd
+
+sudo dockerd > /dev/null 2>&1 & disown
+
+
 sudo docker run \
  -p "27017:27017" \
  -v /home/ec2-user/mongo/data:/data/db \
   mongo   > /dev/null 2>&1 & disown
+
+
