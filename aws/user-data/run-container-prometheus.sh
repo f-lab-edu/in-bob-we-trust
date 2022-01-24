8361f25f1604869799f3a92ec567f785d6d59804
Kim, Joo Hyuk <beanskobe@gmail.com>
2022-01-25 01:45:38 +0900

    #172 Observability 서비스 적용 (#178)
    * Chores : script files 남기기
    
    * Refactor : AWS SecretsManager 설정 파일 삭제
    
    * Feat : Zipkin + Spring Cloud Sleuth 설정정보 추가
    
    * Feat : apply Observability (Zipkin && Sleuth)
    
    * Fix : Prometheus add relay-service

---
+++ aws/user-data/run-container-prometheus.sh
@@ -0,0 +1,10 @@
+sudo yum update
+
+sudo yum install dockerd
+
+sudo dockerd > /dev/null 2>&1 & disown
+
+sudo docker run \
+  --network=host \
+  -v $(pwd)/prometheus.yml:/etc/prometheus/prometheus.yml \
+  prom/prometheus  > /dev/null 2>&1 & disown
