5078d01d56a70f17132cda4dd1be80a3bd882133
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-24 23:49:18 +0900

    Test : remove unused

--- delivery-relay-service/src/test/java/com/inbobwetrust/DeliveryRelayServiceApplicationTest.java
+++ delivery-relay-service/src/test/java/com/inbobwetrust/DeliveryRelayServiceApplicationTest.java
@@ -6,43 +6,5 @@ import org.junit.jupiter.api.Test;
 import org.springframework.util.SocketUtils;
 
 public class DeliveryRelayServiceApplicationTest {
-  int serverPort;
 
-  @BeforeEach
-  void setUp() {
-    System.getProperties().put("server.port", SocketUtils.findAvailableTcpPort());
-  }
-
-  @Test
-  void defaultProfileTest() {
-    Assertions.assertDoesNotThrow(
-        () -> {
-          DeliveryRelayServiceApplication.main(new String[] {});
-        });
-  }
-
-  @Test
-  void productionProfileTest() {
-    Assertions.assertDoesNotThrow(
-        () -> {
-          DeliveryRelayServiceApplication.main(
-              new String[] {"--spring.profiles.active=production"});
-        });
-  }
-
-  @Test
-  void localProfileTest() {
-    Assertions.assertDoesNotThrow(
-        () -> {
-          DeliveryRelayServiceApplication.main(new String[] {"--spring.profiles.active=local"});
-        });
-  }
-
-  @Test
-  void testProfileTest() {
-    Assertions.assertDoesNotThrow(
-        () -> {
-          DeliveryRelayServiceApplication.main(new String[] {"--spring.profiles.active=test"});
-        });
-  }
 }
