127d3b75b797961088ef281dcd6c49b385cbce38
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-26 15:54:37 +0900

    Test : K6 Full 유저스크립트 완성

--- k6-test/script.js
+++ k6-test/script.js
@@ -3,22 +3,22 @@ import http from 'k6/http';
 import {textSummary} from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 
-export let options = {
-    vus:1,
-    duration:'1s',
+// export let options = {
+//     vus: 1,
+//     duration: '1s',
+//
+// }
 
-}
-/*
 export let options = {
-    vus: 100,
+    vus: 1,
     startTime: '10s', // the ramping API test starts a little later
-    startRate: 10,
+    startRate: 1,
     timeUnit: '1s', // we start at 50 iterations per second
     stages: [
-        {duration: '30s', target: 100},
-        {duration: '30s', target: 300},
-        {duration: '30s', target: 500},
-        {duration: '30s', target: 100}, // below normal load
+        {duration: '300s', target: 30},
+        {duration: '300s', target: 10},
+        {duration: '300s', target: 100},
+        {duration: '300s', target: 100},
     ],
     thresholds: {
         // http errors should be less than 1%
@@ -29,7 +29,7 @@ export let options = {
         checks: ['rate>=0.95']
     }
 };
-
+/*
 */
 
 const params = {
@@ -43,30 +43,34 @@ export default () => {
     const minWaitTime = 0.00;
     const maxWaitTime = 1.00;
 
-    const uri = "http://localhost:8888";
+    
+    const uri = "http://localhost:8888"
     const URI = uri + "/api/delivery"
 
-    const ORDER_ID = new Date().toISOString();
-
     // 신규주문
-    const req_addDelivery = makeDelivery(ORDER_ID, 'NEW');
+    const req_addDelivery = makeNewDelivery();
+    req_addDelivery['riderId'] = null;
     const addDelivery = http.post(URI, JSON.stringify(req_addDelivery), params);
     check(addDelivery, {
         'addDelivery is OK 200': () => {
             if (addDelivery.status !== 200) {
                 console.info('addDelivery result >>> ' + addDelivery.body);
+                fail();
             }
             return addDelivery.status === 200;
         }
     });
 
-    sleep(generateRandomNumberBetween(minWaitTime, maxWaitTime));
+    sleep(1);
 
+    const SAVED_DELIVERY = JSON.parse(addDelivery.body);
+    const DELIVRY_ID = SAVED_DELIVERY.id;
 
     // 주문접수
-    const req_acceptDelivery = makeDelivery(ORDER_ID, 'ACCEPTED');
+    const req_acceptDelivery = makeDelivery(DELIVRY_ID, 'ACCEPTED');
+    req_acceptDelivery['riderId'] = null;
     req_acceptDelivery['orderTime'] = new Date().toISOString();
-    sleep(0.1)
+    sleep(0.01);
     req_acceptDelivery['pickupTime'] = new Date().toISOString();
 
     const acceptDelivery = http.put(URI + "/accept", JSON.stringify(req_acceptDelivery), params);
@@ -74,31 +78,33 @@ export default () => {
         'acceptDelivery is OK 200': () => {
             if (acceptDelivery.status !== 200) {
                 console.info('acceptDelivery result >>> ' + acceptDelivery.body);
+                fail();
             }
             return acceptDelivery.status === 200;
         }
     });
-    sleep(generateRandomNumberBetween(minWaitTime, maxWaitTime));
+    sleep(1);
 
 
     // 라이더 배정
-    const req_setDeliveryRider = makeDelivery(ORDER_ID, 'ACCEPTED');
-    req_setDeliveryRider['riderId'] = null;
+    const req_setDeliveryRider = makeDelivery(DELIVRY_ID, 'ACCEPTED');
+
 
     const setDeliveryRider = http.put(URI + "/rider", JSON.stringify(req_setDeliveryRider), params);
     check(setDeliveryRider, {
         'setDeliveryRider is OK 200': () => {
             if (setDeliveryRider.status !== 200) {
                 console.info('setDeliveryRider result >>> ' + setDeliveryRider.body);
+                fail();
             }
             return setDeliveryRider.status === 200;
         }
     });
 
-    sleep(generateRandomNumberBetween(minWaitTime, maxWaitTime));
+    sleep(1);
 
     // 픽업완료
-    const req_setPickedUp = makeDelivery(ORDER_ID, 'PICKED_UP');
+    const req_setPickedUp = makeDelivery(DELIVRY_ID, 'PICKED_UP');
     req_setPickedUp['deliveryStatus'] = 'PICKED_UP';
 
     const setPickedUp = http.put(URI + "/pickup", JSON.stringify(req_setPickedUp), params);
@@ -106,29 +112,48 @@ export default () => {
         'setPickedUp is OK 200': () => {
             if (setPickedUp.status !== 200) {
                 console.info('setPickedUp result >>> ' + setPickedUp.body);
+                fail();
             }
             return setPickedUp.status === 200;
         }
     });
 
-    sleep(generateRandomNumberBetween(minWaitTime, maxWaitTime));
+    sleep(1);
 
 
     // 배달완료
-    const req_setComplete = makeDelivery(ORDER_ID, 'COMPLETE');
+    const req_setComplete = makeDelivery(DELIVRY_ID, 'COMPLETE');
 
     const setComplete = http.put(URI + "/complete", JSON.stringify(req_setComplete), params);
     check(setComplete, {
         'setComplete is OK 200': () => {
             if (setComplete.status !== 200) {
                 console.info('setComplete result >>> ' + setComplete.body);
+                fail();
             }
             return setComplete.status === 200;
         }
     });
 
-    sleep(generateRandomNumberBetween(minWaitTime, maxWaitTime));
+    sleep(1);
 };
+
+
+function makeNewDelivery(time) {
+    return {
+        'orderId': 'orderId-' + time,
+        'agencyId': 'agencyId-' + time,
+        'shopId': 'shopId-' + time,
+        'customerId': 'customerId-' + time,
+        'address': 'address-' + time,
+        'phoneNumber': 'phoneNumber-' + time,
+        'comment': 'comment-' + time,
+        'deliveryStatus': 'NEW',
+        'orderTime': new Date().toISOString(),
+        'pickupTime': new Date().toISOString(),
+        'finishTime': new Date().toISOString()
+    }
+}
 
 function makeDelivery(id, status) {
     return {
