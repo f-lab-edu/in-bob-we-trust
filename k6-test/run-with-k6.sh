edcd448fb0a311516e5a193fc5c5f464b37c8e3e
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-27 20:02:27 +0900

    Chores : some useful shell scripts

--- k6-test/run-with-k6.sh
+++ k6-test/run-with-k6.sh
@@ -1,6 +1,5 @@
-DIR=/Users/joohyuk/dev/github.com/temp/in-bob-we-trust/k6-test
-
-cp $DIR/script.js /users/joohyuk/dev/github.com/k6/samples/script.js
+rm -f /users/joohyuk/dev/github.com/k6/samples/script.js
+cp /Users/joohyuk/dev/github.com/temp/in-bob-we-trust/k6-test/script.js /users/joohyuk/dev/github.com/k6/samples/script.js
 
 cd /users/joohyuk/dev/github.com/k6 || exit
 
