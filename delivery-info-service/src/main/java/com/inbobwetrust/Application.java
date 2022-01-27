0752444b11e676b0b11e045e3bc6a4a7032c681f
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-27 22:22:32 +0900

    Feat : blockhound

--- delivery-info-service/src/main/java/com/inbobwetrust/Application.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/Application.java
@@ -1,12 +1,24 @@
 package com.inbobwetrust;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
   public static void main(String[] args) {
+    BlockHound.install();
     SpringApplication.run(Application.class, args);
+  }
+
+
+  @PostConstruct
+  public void init() {
+    log.info("CPU: {}", Runtime.getRuntime().availableProcessors());
   }
 }
