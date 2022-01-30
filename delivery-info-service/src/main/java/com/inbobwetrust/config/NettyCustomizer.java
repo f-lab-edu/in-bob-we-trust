6a7b265a80bd381db9ada6c4f962b22d14f6cd55
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-30 23:11:08 +0900

    Feat : set NioEventloopGroup size 1

--- delivery-info-service/src/main/java/com/inbobwetrust/config/NettyCustomizer.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/config/NettyCustomizer.java
@@ -1,33 +1,33 @@
-// package com.inbobwetrust.config;
-//
-// import io.netty.channel.EventLoopGroup;
-// import io.netty.channel.nio.NioEventLoopGroup;
-// import io.netty.channel.socket.nio.NioServerSocketChannel;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
-// import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import reactor.netty.http.server.HttpServer;
-//
-// @Configuration
-// @Slf4j
-// public class NettyCustomizer {
-//
-//  @Bean
-//  public NettyReactiveWebServerFactory nettyReactiveWebServerFactory() {
-//    var webServerFactory = new NettyReactiveWebServerFactory();
-//    webServerFactory.addServerCustomizers(new EventLoopNettyCustomizer());
-//    return webServerFactory;
-//  }
-//
-//  private static class EventLoopNettyCustomizer implements NettyServerCustomizer {
-//
-//    @Override
-//    public HttpServer apply(HttpServer httpServer) {
-//      EventLoopGroup eventLoopGroup = new NioEventLoopGroup(3);
-//      eventLoopGroup.register(new NioServerSocketChannel());
-//      return httpServer.runOn(eventLoopGroup);
-//    }
-//  }
-// }
+package com.inbobwetrust.config;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.LoopResources;
+import reactor.netty.tcp.TcpServer;
+
+import java.util.Collections;
+
+@Configuration
+@Slf4j
+public class NettyCustomizer {
+
+  @Bean
+  public NioEventLoopGroup nioEventLoopGroup() {
+    return new NioEventLoopGroup(1);
+  }
+
+  @Bean
+  public NettyReactiveWebServerFactory factory(NioEventLoopGroup eventLoopGroup) {
+    NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
+    factory.setServerCustomizers(
+        Collections.singletonList(httpServer -> httpServer.runOn(eventLoopGroup)));
+    return factory;
+  }
+}
