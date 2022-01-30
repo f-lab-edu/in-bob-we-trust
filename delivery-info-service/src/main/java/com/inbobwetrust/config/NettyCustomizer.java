805952f45f47375a9841ffe39c0f168b251fbebe
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-01-30 23:27:49 +0900

    Test : run on 1 thread

--- delivery-info-service/src/main/java/com/inbobwetrust/config/NettyCustomizer.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/config/NettyCustomizer.java
@@ -1,13 +1,17 @@
 package com.inbobwetrust.config;
 
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
 import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.server.WebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.HttpHandler;
 import reactor.netty.http.server.HttpServer;
 import reactor.netty.resources.LoopResources;
 import reactor.netty.tcp.TcpServer;
@@ -19,15 +23,20 @@ import java.util.Collections;
 public class NettyCustomizer {
 
   @Bean
-  public NioEventLoopGroup nioEventLoopGroup() {
-    return new NioEventLoopGroup(1);
+  public NettyReactiveWebServerFactory nettyReactiveWebServerFactory() {
+    var webServerFactory = new NettyReactiveWebServerFactory();
+    webServerFactory.addServerCustomizers(new EventLoopNettyCustomizer());
+    return webServerFactory;
   }
 
-  @Bean
-  public NettyReactiveWebServerFactory factory(NioEventLoopGroup eventLoopGroup) {
-    NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
-    factory.setServerCustomizers(
-        Collections.singletonList(httpServer -> httpServer.runOn(eventLoopGroup)));
-    return factory;
+  private static class EventLoopNettyCustomizer implements NettyServerCustomizer {
+    @Override
+    public HttpServer apply(HttpServer httpServer) {
+      int nthreads = 1;
+      log.info("configured........ {}", nthreads);
+      EventLoopGroup eventLoopGroup = new NioEventLoopGroup(nthreads);
+      eventLoopGroup.register(new NioServerSocketChannel());
+      return httpServer.runOn(eventLoopGroup);
+    }
   }
 }
