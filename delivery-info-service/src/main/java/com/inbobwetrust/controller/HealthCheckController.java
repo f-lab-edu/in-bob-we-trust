ef94ee4451d4bdff670d919fc563f41b2c7ff925
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-02-07 15:25:09 +0900

    Feat : IP 주소확인 엔드포인트 추가

--- delivery-info-service/src/main/java/com/inbobwetrust/controller/HealthCheckController.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/controller/HealthCheckController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
@@ -20,5 +23,21 @@ public class HealthCheckController {
   @GetMapping
   Mono<String> helloWorld() {
     return Mono.just(helloWorld);
+  }
+
+  String IP_PREFIX = "IP is...";
+  String ipMessage = IP_PREFIX;
+
+  @GetMapping("/ip")
+  Mono<String> getIp() {
+    if (ipMessage.equals(IP_PREFIX)) {
+      try {
+        InetAddress ip = InetAddress.getLocalHost();
+        ipMessage += " From host: " + ip;
+      } catch (UnknownHostException e) {
+        e.printStackTrace();
+      }
+    }
+    return Mono.just(ipMessage);
   }
 }
