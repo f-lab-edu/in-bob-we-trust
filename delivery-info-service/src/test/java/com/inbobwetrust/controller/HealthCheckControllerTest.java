af0793ab74082ca900ad0e0ee6d676ab90457f12
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-02-11 20:37:15 +0900

    Fix : Java compile error (duplicate test method)

--- delivery-info-service/src/test/java/com/inbobwetrust/controller/HealthCheckControllerTest.java
+++ delivery-info-service/src/test/java/com/inbobwetrust/controller/HealthCheckControllerTest.java
@@ -1,15 +1,16 @@
 package com.inbobwetrust.controller;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import groovy.util.logging.Slf4j;
-import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @WebFluxTest(HealthCheckController.class)
 @AutoConfigureWebTestClient
@@ -38,29 +39,6 @@ class HealthCheckControllerTest {
                       .toLowerCase()
                       .contains("hello world"));
             });
-    // then
-  }
-
-  @Test
-  @DisplayName("Ping Hello World")
-  void helloWorld() {
-    // given
-    var uri = "";
-    // when
-    testClient
-      .get()
-      .uri(uri)
-      .exchange()
-      .expectStatus()
-      .isOk()
-      .expectBody(String.class)
-      .consumeWith(
-        response -> {
-          assertTrue(
-            Objects.requireNonNull(response.getResponseBody())
-              .toLowerCase()
-              .contains("hello world"));
-        });
     // then
   }
 }
