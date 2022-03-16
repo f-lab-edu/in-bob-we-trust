7fffc76b3c73e078b743deb204bbad926c20168e
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-16 09:42:00 +0900

    Feat : DeliveryPublisher 구현체 추가

--- delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImpl.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImpl.java
@@ -14,39 +14,47 @@ public class DeliveryMessagePublisherImpl implements DeliveryPublisher {
   private String agencyExchange;
 
   public DeliveryMessagePublisherImpl(
-      AmqpTemplate messageQueue, String shopExchange, String agencyExchange) {
+    AmqpTemplate messageQueue, String shopExchange, String agencyExchange) {
     this.messageQueue = messageQueue;
     this.shopExchange = shopExchange;
+    this.agencyExchange = agencyExchange;
+  }
+
+  public void setShopExchange(String shopExchange) {
+    this.shopExchange = shopExchange;
+  }
+
+  public void setAgencyExchange(String agencyExchange) {
     this.agencyExchange = agencyExchange;
   }
 
   @Override
   public Mono<Delivery> sendAddDeliveryEvent(Delivery delivery) {
     return Mono.just(delivery)
-        .subscribeOn(Schedulers.boundedElastic())
-        .flatMap(this::publishAddDeliveryEvent);
+      .subscribeOn(Schedulers.boundedElastic())
+      .flatMap(this::publishAddDeliveryEvent);
   }
 
   private Mono<Delivery> publishAddDeliveryEvent(Delivery delivery) {
     return Mono.fromCallable(
-        () -> {
-          this.messageQueue.convertAndSend(shopExchange, delivery);
-          return delivery;
-        });
+      () -> {
+        this.messageQueue.convertAndSend(shopExchange, delivery);
+        return delivery;
+      });
   }
 
   @Override
   public Mono<Delivery> sendSetRiderEvent(Delivery delivery) {
     return Mono.just(delivery)
-        .subscribeOn(Schedulers.boundedElastic())
-        .flatMap(this::publishSetRiderEvent);
+      .subscribeOn(Schedulers.boundedElastic())
+      .flatMap(this::publishSetRiderEvent);
   }
 
   private Mono<Delivery> publishSetRiderEvent(Delivery delivery) {
     return Mono.fromCallable(
-        () -> {
-          this.messageQueue.convertAndSend(agencyExchange, delivery);
-          return delivery;
-        });
+      () -> {
+        this.messageQueue.convertAndSend(agencyExchange, delivery);
+        return delivery;
+      });
   }
 }
