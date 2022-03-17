cb0cb6d2e0816f917b6dde80756206496e33d74b
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-17 22:12:28 +0900

    Feat : MessageProducer 빈등록 + 생성방법 변경

--- delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImpl.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImpl.java
@@ -1,60 +1,53 @@
 package com.inbobwetrust.publisher;
 
 import com.inbobwetrust.domain.Delivery;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class DeliveryMessagePublisherImpl implements DeliveryPublisher {
 
   private final AmqpTemplate messageQueue;
 
-  private String shopExchange;
+  private final String shopExchange = "messageQueue.exchange.shop";
 
-  private String agencyExchange;
+  private final String agencyExchange = "messageQueue.exchange.agency";
 
-  public DeliveryMessagePublisherImpl(
-      AmqpTemplate messageQueue, String shopExchange, String agencyExchange) {
-    this.messageQueue = messageQueue;
-    this.shopExchange = shopExchange;
-    this.agencyExchange = agencyExchange;
-  }
-
-  public void setShopExchange(String shopExchange) {
-    this.shopExchange = shopExchange;
-  }
-
-  public void setAgencyExchange(String agencyExchange) {
-    this.agencyExchange = agencyExchange;
-  }
 
   @Override
   public Mono<Delivery> sendAddDeliveryEvent(Delivery delivery) {
     return Mono.just(delivery)
-        .subscribeOn(Schedulers.boundedElastic())
-        .flatMap(this::publishAddDeliveryEvent);
+      .subscribeOn(Schedulers.boundedElastic())
+      .flatMap(this::publishAddDeliveryEvent);
   }
 
   private Mono<Delivery> publishAddDeliveryEvent(Delivery delivery) {
     return Mono.fromCallable(
-        () -> {
-          this.messageQueue.convertAndSend(shopExchange, delivery);
-          return delivery;
-        });
+      () -> {
+        this.messageQueue.convertAndSend(shopExchange, delivery);
+        return delivery;
+      });
   }
 
   @Override
   public Mono<Delivery> sendSetRiderEvent(Delivery delivery) {
     return Mono.just(delivery)
-        .subscribeOn(Schedulers.boundedElastic())
-        .flatMap(this::publishSetRiderEvent);
+      .subscribeOn(Schedulers.boundedElastic())
+      .flatMap(this::publishSetRiderEvent);
   }
 
   private Mono<Delivery> publishSetRiderEvent(Delivery delivery) {
     return Mono.fromCallable(
-        () -> {
-          this.messageQueue.convertAndSend(agencyExchange, delivery);
-          return delivery;
-        });
+      () -> {
+        this.messageQueue.convertAndSend(agencyExchange, delivery);
+        return delivery;
+      });
   }
 }
+
