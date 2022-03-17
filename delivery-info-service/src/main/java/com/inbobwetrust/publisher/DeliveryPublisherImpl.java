c5bdaba9310d116a8c94ec8b078142ad1b098a45
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-17 22:15:19 +0900

    Chore : google java format 적용

--- delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryPublisherImpl.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/publisher/DeliveryPublisherImpl.java
@@ -7,12 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Component
 @RequiredArgsConstructor
 @Slf4j
 public class DeliveryPublisherImpl implements DeliveryPublisher {
@@ -26,45 +24,44 @@ public class DeliveryPublisherImpl implements DeliveryPublisher {
 
   public Mono<Delivery> sendAddDeliveryEvent(Delivery delivery) {
     return webClient
-        .post()
-        .uri(proxyShopUrl + "/" + delivery.getShopId())
-        .body(Mono.just(delivery), Delivery.class)
-        .retrieve()
-        .onStatus(
-            HttpStatus::is4xxClientError,
-            clientResponse -> this.handleOn4xxStatus(delivery, clientResponse))
-        .onStatus(
-            HttpStatus::is5xxServerError,
-            serverResponse -> this.handleOn5xxStatus(delivery, serverResponse))
-        .bodyToMono(Delivery.class);
+      .post()
+      .uri(proxyShopUrl + "/" + delivery.getShopId())
+      .body(Mono.just(delivery), Delivery.class)
+      .retrieve()
+      .onStatus(
+        HttpStatus::is4xxClientError,
+        clientResponse -> this.handleOn4xxStatus(delivery, clientResponse))
+      .onStatus(
+        HttpStatus::is5xxServerError,
+        serverResponse -> this.handleOn5xxStatus(delivery, serverResponse))
+      .bodyToMono(Delivery.class);
   }
 
-  @Override
   public Mono<Delivery> sendSetRiderEvent(Delivery delivery) {
     return webClient
-        .post()
-        .uri(proxyAgencyUrl + "/" + delivery.getAgencyId())
-        .body(Mono.just(delivery), Delivery.class)
-        .retrieve()
-        .onStatus(
-            HttpStatus::is4xxClientError,
-            clientResponse -> this.handleOn4xxStatus(delivery, clientResponse))
-        .onStatus(
-            HttpStatus::is5xxServerError,
-            serverResponse -> this.handleOn5xxStatus(delivery, serverResponse))
-        .bodyToMono(Delivery.class);
+      .post()
+      .uri(proxyAgencyUrl + "/" + delivery.getAgencyId())
+      .body(Mono.just(delivery), Delivery.class)
+      .retrieve()
+      .onStatus(
+        HttpStatus::is4xxClientError,
+        clientResponse -> this.handleOn4xxStatus(delivery, clientResponse))
+      .onStatus(
+        HttpStatus::is5xxServerError,
+        serverResponse -> this.handleOn5xxStatus(delivery, serverResponse))
+      .bodyToMono(Delivery.class);
   }
 
   private Mono<? extends Throwable> handleOn5xxStatus(
-      Delivery delivery, ClientResponse serverResponse) {
+    Delivery delivery, ClientResponse serverResponse) {
     log.info("Status code 5XX is : {}", serverResponse.statusCode().value());
     log.info("Error Status 5XX for delivery : {}", delivery);
     return Mono.error(
-        new RelayServerException("Shop operation failed for delivery :     " + delivery));
+      new RelayServerException("Shop operation failed for delivery :     " + delivery));
   }
 
   private Mono<? extends Throwable> handleOn4xxStatus(
-      Delivery delivery, ClientResponse clientResponse) {
+    Delivery delivery, ClientResponse clientResponse) {
     log.info("Status code 4xx is : {}", clientResponse.statusCode().value());
     log.info("Error Status 4XX for delivery : {}", delivery);
     return Mono.error(new RelayClientException("Push Event failed for delivery :     " + delivery));
