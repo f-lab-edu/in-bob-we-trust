e7b5dfd5f477adf1039c5afc62cf4714eb1911f1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-17 22:34:42 +0900

    Fix : syntax error

--- delivery-info-service/src/integrationTest/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImplTest.java
+++ delivery-info-service/src/integrationTest/java/com/inbobwetrust/publisher/DeliveryMessagePublisherImplTest.java
@@ -3,6 +3,8 @@ package com.inbobwetrust.publisher;
 import com.inbobwetrust.domain.Delivery;
 import com.inbobwetrust.repository.DeliveryRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +18,12 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDateTime;
+
+import static org.codehaus.groovy.runtime.DefaultGroovyMethods.any;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
@@ -53,15 +61,15 @@ public class DeliveryMessagePublisherImplTest {
     var delivery = makeValidDelivery();
     // when
     var resBody = this.webTestClient
-        .post()
-        .uri("/api/delivery")
-        .bodyValue(delivery)
-        .exchange()
-        .expectStatus()
-        .isOk()
-        .expectBody(Delivery.class)
-        .returnResult()
-        .getResponseBody();
+      .post()
+      .uri("/api/delivery")
+      .bodyValue(delivery)
+      .exchange()
+      .expectStatus()
+      .isOk()
+      .expectBody(Delivery.class)
+      .returnResult()
+      .getResponseBody();
 
     Thread.sleep(1000L);
     // then
@@ -69,17 +77,17 @@ public class DeliveryMessagePublisherImplTest {
     delivery.setId(savedDelivery.getId());
     assertTrue(delivery.getShopId().equals(savedDelivery.getShopId()));
     assertTrue(delivery.getCustomerId().equals(savedDelivery.getCustomerId()));
-    verify(amqpTemplate, times(1)).convertAndSend(eq(shopExchange), any(Delivery.class));
+    Mockito.verify(amqpTemplate, times(1)).convertAndSend(ArgumentMatchers.eq(shopExchange), any(Delivery.class));
   }
 
   private Delivery makeValidDelivery() {
     return Delivery.builder()
-        .shopId("shop1234")
-        .orderId("order-1234")
-        .customerId("customer-1234")
-        .address("서울시 강남구 삼성동 봉은사로 12-41")
-        .phoneNumber("01031583212")
-        .orderTime(LocalDateTime.now())
-        .build();
+      .shopId("shop1234")
+      .orderId("order-1234")
+      .customerId("customer-1234")
+      .address("서울시 강남구 삼성동 봉은사로 12-41")
+      .phoneNumber("01031583212")
+      .orderTime(LocalDateTime.now())
+      .build();
   }
 }
