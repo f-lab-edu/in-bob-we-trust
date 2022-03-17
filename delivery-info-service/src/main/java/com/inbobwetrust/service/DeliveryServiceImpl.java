f56256939f1971e8e5c4566011fa4a56edd1cfe1
Kim, Joo Hyuk <61615301+JooHyukKim@users.noreply.github.com>
2022-03-17 22:14:10 +0900

    Chore : format 탭 수정

--- delivery-info-service/src/main/java/com/inbobwetrust/service/DeliveryServiceImpl.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/service/DeliveryServiceImpl.java
@@ -1,24 +1,23 @@
 package com.inbobwetrust.service;
 
-import static com.inbobwetrust.domain.DeliveryStatus.ACCEPTED;
-import static com.inbobwetrust.domain.DeliveryStatus.PICKED_UP;
-
 import com.inbobwetrust.domain.Delivery;
 import com.inbobwetrust.domain.DeliveryStatus;
 import com.inbobwetrust.exception.DeliveryNotFoundException;
 import com.inbobwetrust.publisher.DeliveryPublisher;
 import com.inbobwetrust.repository.DeliveryRepository;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.RetryBackoffSpec;
 
-@Service
-@Slf4j
+import java.util.Objects;
+
+import static com.inbobwetrust.domain.DeliveryStatus.ACCEPTED;
+import static com.inbobwetrust.domain.DeliveryStatus.PICKED_UP;
+
+@Component
 @RequiredArgsConstructor
 public class DeliveryServiceImpl implements DeliveryService {
   private final DeliveryRepository deliveryRepository;
@@ -32,46 +31,46 @@ public class DeliveryServiceImpl implements DeliveryService {
   @Override
   public Mono<Delivery> acceptDelivery(Delivery delivery) {
     return Mono.just(delivery)
-        .flatMap(DeliveryValidator::statusIsNotNull)
-        .flatMap(DeliveryValidator::statusIsAccepted)
-        .flatMap(DeliveryValidator::pickupTimeIsAfterOrderTime)
-        .flatMap(del -> deliveryRepository.save(delivery))
-        .flatMap(deliveryPublisher::sendSetRiderEvent);
+      .flatMap(DeliveryValidator::statusIsNotNull)
+      .flatMap(DeliveryValidator::statusIsAccepted)
+      .flatMap(DeliveryValidator::pickupTimeIsAfterOrderTime)
+      .flatMap(del -> deliveryRepository.save(delivery))
+      .flatMap(deliveryPublisher::sendSetRiderEvent);
   }
 
   @Override
   public Mono<Delivery> setDeliveryRider(Delivery delivery) {
     return deliveryRepository
-        .findById(delivery.getId())
-        .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
-        .flatMap(DeliveryValidator::canSetDeliveryRider)
-        .flatMap(del -> deliveryRepository.save(delivery));
+      .findById(delivery.getId())
+      .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
+      .flatMap(DeliveryValidator::canSetDeliveryRider)
+      .flatMap(del -> deliveryRepository.save(delivery));
   }
 
   @Override
   public Mono<Delivery> setPickedUp(Delivery newDelivery) {
     return deliveryRepository
-        .findById(newDelivery.getId())
-        .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
-        .flatMap(existingDelivery -> DeliveryValidator.canSetPickUp(existingDelivery, newDelivery))
-        .flatMap(del -> deliveryRepository.save(newDelivery));
+      .findById(newDelivery.getId())
+      .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
+      .flatMap(existingDelivery -> DeliveryValidator.canSetPickUp(existingDelivery, newDelivery))
+      .flatMap(del -> deliveryRepository.save(newDelivery));
   }
 
   @Override
   public Mono<Delivery> setComplete(Delivery newDelivery) {
     return deliveryRepository
-        .findById(newDelivery.getId())
-        .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
-        .flatMap(
-            existingDelivery -> DeliveryValidator.canSetComplete(existingDelivery, newDelivery))
-        .flatMap(del -> deliveryRepository.save(newDelivery));
+      .findById(newDelivery.getId())
+      .switchIfEmpty(Mono.error(DeliveryNotFoundException::new))
+      .flatMap(
+        existingDelivery -> DeliveryValidator.canSetComplete(existingDelivery, newDelivery))
+      .flatMap(del -> deliveryRepository.save(newDelivery));
   }
 
   @Override
   public Mono<Delivery> findById(String id) {
     return deliveryRepository
-        .findById(id)
-        .switchIfEmpty(Mono.error(DeliveryNotFoundException::new));
+      .findById(id)
+      .switchIfEmpty(Mono.error(DeliveryNotFoundException::new));
   }
 
   @Override
@@ -87,9 +86,9 @@ public class DeliveryServiceImpl implements DeliveryService {
   @Override
   public Mono<Boolean> isPickedUp(String id) {
     return deliveryRepository
-        .findById(id)
-        .flatMap(del -> Mono.just(del.getDeliveryStatus().equals(PICKED_UP)))
-        .onErrorReturn(NullPointerException.class, false);
+      .findById(id)
+      .flatMap(del -> Mono.just(del.getDeliveryStatus().equals(PICKED_UP)))
+      .onErrorReturn(NullPointerException.class, false);
   }
 
   public static final String MSG_RIDER_ALREADY_SET = "배정된 라이더가 존재합니다. 라이더ID : ";
@@ -116,9 +115,9 @@ public class DeliveryServiceImpl implements DeliveryService {
     private static Mono<Delivery> canSetPickUp(Delivery before, Delivery after) {
       if (!canUpdateStatus(ACCEPTED, before, after)) {
         String message =
-            String.format(
-                "픽업완료로 전환이 불가한 상태입니다.     기존주문상태: %s       요청한주문상태: %s",
-                before.getDeliveryStatus(), after.getDeliveryStatus());
+          String.format(
+            "픽업완료로 전환이 불가한 상태입니다.     기존주문상태: %s       요청한주문상태: %s",
+            before.getDeliveryStatus(), after.getDeliveryStatus());
         return Mono.error(new IllegalStateException(message));
       }
       return Mono.just(after);
@@ -127,16 +126,16 @@ public class DeliveryServiceImpl implements DeliveryService {
     private static Mono<Delivery> canSetComplete(Delivery before, Delivery after) {
       if (!canUpdateStatus(PICKED_UP, before, after)) {
         String message =
-            String.format(
-                "배달완료로 전환이 불가한 상태입니다.     기존주문상태: %s       요청한주문상태: %s",
-                before.getDeliveryStatus(), after.getDeliveryStatus());
+          String.format(
+            "배달완료로 전환이 불가한 상태입니다.     기존주문상태: %s       요청한주문상태: %s",
+            before.getDeliveryStatus(), after.getDeliveryStatus());
         return Mono.error(new IllegalStateException(message));
       }
       return Mono.just(after);
     }
 
     private static boolean canUpdateStatus(
-        DeliveryStatus expectedBeforeStatus, Delivery before, Delivery after) {
+      DeliveryStatus expectedBeforeStatus, Delivery before, Delivery after) {
       boolean statusSameAsExpected = before.getDeliveryStatus().equals(expectedBeforeStatus);
       boolean isNext = before.getDeliveryStatus().getNext().equals(after.getDeliveryStatus());
       return statusSameAsExpected && isNext;
@@ -144,26 +143,26 @@ public class DeliveryServiceImpl implements DeliveryService {
 
     private static Mono<Delivery> monoJustOrError(Delivery after, StringBuilder errorMessage) {
       return errorMessage.length() == 0
-          ? Mono.just(after)
-          : Mono.error(new IllegalArgumentException(errorMessage.toString()));
+        ? Mono.just(after)
+        : Mono.error(new IllegalArgumentException(errorMessage.toString()));
     }
 
     private static Mono<Delivery> statusIsAccepted(Delivery delivery) {
       return delivery.getDeliveryStatus().equals(DeliveryStatus.ACCEPTED)
-          ? Mono.just(delivery)
-          : Mono.error(new IllegalStateException("주문상태가 ACCEPTED가 아닙니다"));
+        ? Mono.just(delivery)
+        : Mono.error(new IllegalStateException("주문상태가 ACCEPTED가 아닙니다"));
     }
 
     private static Mono<Delivery> statusIsNotNull(Delivery delivery) {
       return Objects.isNull(delivery.getDeliveryStatus())
-          ? Mono.error(new IllegalStateException("주문상태가 null 입니다."))
-          : Mono.just(delivery);
+        ? Mono.error(new IllegalStateException("주문상태가 null 입니다."))
+        : Mono.just(delivery);
     }
 
     private static Mono<Delivery> pickupTimeIsAfterOrderTime(Delivery delivery) {
       return delivery.getPickupTime().isAfter(delivery.getOrderTime())
-          ? Mono.just(delivery)
-          : Mono.error(new IllegalStateException("픽업시간은 주문시간 이후여야 합니다."));
+        ? Mono.just(delivery)
+        : Mono.error(new IllegalStateException("픽업시간은 주문시간 이후여야 합니다."));
     }
   }
 }
