bc5dcc7a19da3e18e6e35637458b0a4619aaf139
Kim, Joo Hyuk <beanskobe@gmail.com>
2022-03-18 15:29:57 +0900

    #Refactor 메시지큐에 사용되는 MessageConverter 빈 타입 추상화 (#207)

--- delivery-info-service/src/main/java/com/inbobwetrust/config/Jack2JsonConfiguration.java
+++ delivery-info-service/src/main/java/com/inbobwetrust/config/Jack2JsonConfiguration.java
@@ -3,6 +3,7 @@ package com.inbobwetrust.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class Jack2JsonConfiguration {
 
   @Bean
-  public Jackson2JsonMessageConverter jsonMessageConverter() {
+  public MessageConverter jsonMessageConverter() {
     return new Jackson2JsonMessageConverter(
-        new ObjectMapper().registerModule(new JavaTimeModule()));
+      new ObjectMapper().registerModule(new JavaTimeModule()));
   }
 }
